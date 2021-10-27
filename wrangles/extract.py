f051016f115522b1f5056996e223cad1f72d63e1
Chris Ince <chris@ince.live>
2021-10-27 15:19:34 -0500

    Add extract geography wrangle

--- wrangles/extract.py
+++ wrangles/extract.py
@@ -6,8 +6,8 @@ import requests
 from . import config as _config
 from . import auth
 from typing import Union
-    
 
+    
 def attributes(input: Union[str, list]) -> list:
     """
     Extract numeric attributes from unstructured text such as lengths or voltages.
@@ -25,6 +25,29 @@ def attributes(input: Union[str, list]) -> list:
     
     if isinstance(input, str): results = results[0]
 
+    return results
+
+
+def geography(input: Union[str, list], dataType: str) -> list:
+    """
+    Extract geographical information from unstructured text such as streets, cities or countries.
+
+    e.g. '1100 Congress Ave, Austin, TX 78701, United States' -> '1100 Congress Ave'
+
+    :param input: A string or list of strings with addresses to search for information.
+    :param dataType: The type of information to return. 'streets', 'cities', 'regions' or 'countries'
+    :return: A list of any results found.
+    """
+    if isinstance(input, str): 
+        json_data = [input]
+    else:
+        json_data = input
+
+    response = requests.post(f'{_config.api_host}/wrangles/extract/geography', params={'responseFormat':'array', 'dataType':dataType }, headers={'Authorization': f'Bearer {auth.get_access_token()}'}, json=json_data)
+    results = response.json()
+
+    if isinstance(input, str): results = results[0]
+    
     return results
 
 
