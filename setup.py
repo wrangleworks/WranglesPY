95ed924e577ab4d7b8a197b6f5ec3e1130d92a73
Chris Ince <chris@wrangleworks.com>
2022-04-27 12:31:29 -0500

    Update setup

--- setup.py
+++ setup.py
@@ -1,13 +1,24 @@
 from setuptools import setup
 
 setup(
-    name='Wrangles',
-    packages=['wrangles', 'wrangles.match'],
-    description='Wrangle your data into shape with machine learning',
-    version='0.2',
-    url='https://github.com/wrangleworks/WranglesPy',
-    author='Wrangleworks',
-    author_email='chris@wrangleworks.com',
-    keywords=['data','wrangling'],
-    install_requires=['requests', 'pyyaml', 'pandas', 'openpyxl', 'pandas_flavor', 'boltons', 'pyodbc']
+    name = 'wrangles',
+    packages = ['wrangles', 'wrangles.match', 'wrangles.connectors'],
+    description = 'Wrangle your data into shape with machine learning',
+    version = '0.2.2',
+    url = 'https://github.com/wrangleworks/WranglesPy',
+    author = 'Wrangleworks',
+    author_email = 'chris@wrangleworks.com',
+    keywords = ['data','wrangling'],
+    install_requires = [
+        'requests',
+        'pyyaml',
+        'pandas',
+        'openpyxl',
+        'pandas_flavor',
+        'boltons',
+        'pyodbc',
+        'sqlalchemy',
+        'pymssql',
+        'psycopg2-binary'
+    ]
 )
