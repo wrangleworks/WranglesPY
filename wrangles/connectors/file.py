fe42929c53bd05ece61850ab34d4f46212728330
Chris Ince <chris@wrangleworks.com>
2022-05-13 16:11:51 -0500

    Updated connectors to new format

--- wrangles/connectors/file.py
+++ wrangles/connectors/file.py
@@ -1,63 +1,93 @@
 """
-Connector to read/write from a local filesystem
+Connector to read & write from the local filesystem
+
+Supports Excel, CSV, JSON and JSONL files.
+
+### Sample Recipes
+~~~
+
+import:
+  file:
+    name: myfile.xlsx
+
+~~~
+
+export:
+  file:
+    name: myfile.csv
+
+~~~
 """
-import pandas
+import pandas as _pd
 
 
-def read(params):
+def input(name: str, fields = None, parameters = {}):
     """
     Import a file as defined by user parameters.
+
     Supports Excel (.xlsx, .xlsx, .xlsm), CSV (.csv, .txt) and JSON (.json) files.
     JSON and CSV may also be gzipped (.csv.gz, .txt.gz, .json.gz) and will be decompressed.
 
     :param params: Dictionary of parameters to define import
     :return: A Pandas dataframe of the imported data.
     """
-    # df = None
-
-    # Add empty parameters dict if user hasn't entered any
-    if 'parameters' not in params: params['parameters'] = {}
-
     # Open appropriate file type
-    if params['name'].split('.')[-1] in ['xlsx', 'xlsm', 'xls']:
-        if 'dtype' not in params['parameters'].keys(): params['parameters']['dtype'] = 'object'
-        df = pandas.read_excel(params['name'], **params['parameters']).fillna('')
-    elif params['name'].split('.')[-1] in ['csv', 'txt'] or '.'.join(params['name'].split('.')[-2:]) in ['csv.gz', 'txt.gz']:
-        df = pandas.read_csv(params['name'], **params['parameters']).fillna('')
-    elif params['name'].split('.')[-1] in ['json'] or '.'.join(params['name'].split('.')[-2:]) in ['json.gz']:
-        df = pandas.read_json(params['name'], **params['parameters']).fillna('')
+    if name.split('.')[-1] in ['xlsx', 'xlsm', 'xls']:
+        if 'dtype' not in parameters.keys(): parameters['dtype'] = 'object'
+        df = _pd.read_excel(name, **parameters).fillna('')
+    elif name.split('.')[-1] in ['csv', 'txt'] or '.'.join(name.split('.')[-2:]) in ['csv.gz', 'txt.gz']:
+        df = _pd.read_csv(name, **parameters).fillna('')
+    elif name.split('.')[-1] in ['json'] or '.'.join(name.split('.')[-2:]) in ['json.gz']:
+        df = _pd.read_json(name, **parameters).fillna('')
+    elif name.split('.')[-1] in ['jsonl'] or '.'.join(name.split('.')[-2:]) in ['jsonl.gz']:
+        # Set lines to true 
+        parameters['lines'] = True
+        # Only records orientation is supported for JSONL
+        parameters['orient'] = 'records'
+        df = _pd.read_json(name, **parameters).fillna('')
 
     # If the user specifies only certain fields, only include those
-    if 'fields' in params.keys(): df = df[params['fields']]
+    if fields is not None: df = df[fields]
 
     return df
 
 
-def write(df, params):
+def output(df, name: str, fields = None, parameters = {}):
     """
-    Export a file as defined by user parameters.
+    Output a file to the local file system as defined by the parameters.
+
     Supports Excel (.xlsx, .xls), CSV (.csv, .txt) and JSON (.json) files.
     JSON and CSV may also be gzipped (.csv.gz, .txt.gz, .json.gz) and will be compressed.
 
     :param df: Dataframe to be written to a file
-    :param params: Dictionary of parameters that define the file to write
+    :param name: Name of the output file
+    :param fields: (Optional) Subset of the fields to be written. If not provided, all fields will be output
+    :param parameters: Dictionary of parameters to pass to the respective pandas function
     """
     # Add empty parameters dict if user hasn't entered any
-    if 'parameters' not in params: params['parameters'] = {}
+    # if 'parameters' not in params: params['parameters'] = {}
 
     # Select only specific fields if user requests them
-    if 'fields' in params.keys(): df = df[params['fields']]
+    if fields is not None: df = df[fields]
 
     # Write appropriate file
-    if params['name'].split('.')[-1] in ['xlsx', 'xls']:
+    if name.split('.')[-1] in ['xlsx', 'xls']:
+        # Write an Excel file
         # Default to not including index if user hasn't explicitly requested it
-        if 'index' not in params['parameters'].keys(): params['parameters']['index'] = False
-        df.to_excel(params['name'], **params['parameters'])
-    elif params['name'].split('.')[-1] in ['csv', 'txt'] or '.'.join(params['name'].split('.')[-2:]) in ['csv.gz', 'txt.gz']:
+        if 'index' not in parameters.keys(): parameters['index'] = False
+        df.to_excel(name, **parameters)
+    elif name.split('.')[-1] in ['csv', 'txt'] or '.'.join(name.split('.')[-2:]) in ['csv.gz', 'txt.gz']:
+        # Write a CSV file
         # Default to not including index if user hasn't explicitly requested it
-        if 'index' not in params['parameters'].keys(): params['parameters']['index'] = False
-        df.to_csv(params['name'], **params['parameters'])
-    elif params['name'].split('.')[-1] in ['json'] or '.'.join(params['name'].split('.')[-2:]) in ['json.gz']:
-        df.to_json(params['name'], **params['parameters'])
-
-    # return df
+        if 'index' not in parameters.keys(): parameters['index'] = False
+        df.to_csv(name, **parameters)
+    elif name.split('.')[-1] in ['json'] or '.'.join(name.split('.')[-2:]) in ['json.gz']:
+        # Write a JSON file
+        df.to_json(name, **parameters)
+    elif name.split('.')[-1] in ['jsonl'] or '.'.join(name.split('.')[-2:]) in ['jsonl.gz']:
+        # Write a line delimited JSONL file
+        # Set lines to true 
+        parameters['lines'] = True
+        # Only records orientation is supported for JSONL
+        parameters['orient'] = 'records'
+        df.to_json(name, **parameters)
--- wrangles/connectors/sql.py
+++ wrangles/connectors/sql.py
@@ -1,24 +1,65 @@
 """
 Connector to read/write from SQL Database.
 """
-import pandas
+import pandas as _pd
 
 
-def read(params):
+def input(type: str, host: str, user: str, password: str, command: str, port = None, database: str = '', fields = None):
     """
     Import data from a SQL database.
 
-    :param params: Dictionary of parameters to define import
+    :param type: Type of SQL database - mssql or postgres
+    :param host: Hostname or IP of the database
+    :param user: User with access to the database
+    :param password: Password of user
+    :param database: Database to be exported to
+    :param table: Table to be exported to
+    :param port: (Optional) If not provided, the default port for the respective SQL type will be used
     :return: Pandas Dataframe of the imported data
     """
-    if params['type'] == 'mssql':
-        conn = f"mssql+pymssql://{params['user']}:{params['password']}@{params['host']}:{params.get('port', '1433')}/{params.get('database', '')}?charset=utf8"
-    elif params['type'] == 'postgres':
-        conn = f"postgresql+psycopg2://{params['user']}:{params['password']}@{params['host']}:{params.get('port', '5432')}/{params['database']}"
-        pass
+    if type == 'mssql':
+        conn = f"mssql+pymssql://{user}:{password}@{host}:{port or 1433}/{database}?charset=utf8"
+    elif type == 'postgres':
+        conn = f"postgresql+psycopg2://{user}:{password}@{host}:{port or 5432}/{database}"
+    # TODO: Add mysql
+    else:
+        raise ValueError('Unknown SQL Database type. Supported types are mssql and postgres.')
 
-    df = pandas.read_sql(params['command'], conn)
+    df = _pd.read_sql(command, conn)
 
-    if 'fields' in params.keys(): df = df[params['fields']]
+    if fields is not None: df = df[fields]
     
     return df
+
+
+def output(df, type: str, host: str, database: str, table: str, user: str, password: str, port = 0, action = 'INSERT', fields = None):
+    """
+    Export data to a SQL database
+
+    :param df: Dataframe to be exported
+    :param type: Type of SQL database - mssql or postgres
+    :param host: Hostname or IP of the database
+    :param database: Database to be exported to
+    :param table: Table to be exported to
+    :param user: User with access to the database
+    :param password: Password of user
+    :param action: Only INSERT is supported at this time
+    :param fields: (Optional) Subset of the fields to be written. If not provided, all fields will be output
+    """
+    # Create appropriate connection string
+    if type == 'mssql':
+        conn = f"mssql+pymssql://{user}:{password}@{host}:{port or 1433}/{database}?charset=utf8"
+    elif type == 'postgres':
+        conn = f"postgresql+psycopg2://{user}:{password}@{host}:{port or 5432}/{database}"
+    # TODO: Add mysql
+    else:
+        raise ValueError('Unknown SQL Database type. Supported types are mssql and postgres.')
+
+    # Select only specific fields if user requests them
+    if fields is not None: df = df[fields]
+
+    if action.upper() == 'INSERT':
+        df.to_sql(table, conn, if_exists='append', index=False, method='multi', chunksize=1000)
+    else:
+        # TODO: Add UPDATE AND UPSERT
+        raise ValueError('UPDATE and UPSERT are not implemented yet.')
