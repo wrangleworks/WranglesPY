9b2a05e9be2b8e476223dc23dec87f0d8b08a99d
Chris Ince <chris@wrangleworks.com>
2022-05-19 11:11:20 -0500

    Updated pipeline wrangles

--- wrangles/pipeline_wrangles/format.py
+++ wrangles/pipeline_wrangles/format.py
@@ -11,3 +11,14 @@ def price_breaks(df: _pd.DataFrame, input: list, parameters: dict = {}) -> _pd.D
     """
     df = _pd.concat([df, _format.price_breaks(df[input], parameters['categoryLabel'], parameters['valueLabel'])], axis=1)
     return df
+
+
+def remove_duplicates(df: _pd.DataFrame, input: str, output: str) -> _pd.DataFrame:
+    output_list = []
+    for row in df[input].values.tolist():
+        if isinstance(row, list):
+            output_list.append(list(set(row)))
+        else:
+            output_list.append(row)
+    df[output] = output_list
+    return df
