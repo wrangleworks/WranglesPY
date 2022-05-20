3fda6cd68c7ff669733ebb31c38d2e050ce8713c
Chris Ince <chris@ince.live>
2022-05-19 20:36:49 -0500

    Significant updates

--- wrangles/pipeline_wrangles/convert.py
+++ wrangles/pipeline_wrangles/convert.py
@@ -3,50 +3,53 @@ Functions to convert data formats and representations
 """
 import pandas as _pd
 import json as _json
+from typing import Union as _Union
 
 
-def case(df: _pd.DataFrame, input: str, output: str = None, parameters: dict = {}) -> _pd.DataFrame:
+def case(df: _pd.DataFrame, input: _Union[str, list], output: _Union[str, list] = None, parameters: dict = {}) -> _pd.DataFrame:
     """
-    Change the case of the input
+    Change the case of the input.
 
     ```
     wrangles:
       - convert.case:
           input: column
-          output: new column
-          parameters:
-            case: lower
+          output: new column            # Optional
+          case: lower                   # Optional
     ```
-
-    :param df: Input Dataframe
-    :param input: Input column or list of columns to be operated on
-    :param output: (Optional) Output column or list of columns to save results to. If omitted, columns will be altered in place.
-    :param parameters: Dict of settings - desired case
-    :return: Update Dataframe
+    :param df: Input Dataframe.
+    :param input: Input column or list of columns to be operated on.
+    :param output: (Optional) Output column or list of columns to save results to. Default - input columns will be replaced.
+    :param case: (Optional) Desired case. upper, lower, title or sentence. Default - lower
     """
-    # TODO: enable list or string for input/output
-
     # If output is not specified, overwrite input columns in place
     if output is None: output = input
 
     # Get the requested case, default lower
     desired_case = parameters.get('case', 'lower').lower()
 
-    if desired_case == 'lower':
-        df[output] = df[input].str.lower()
-    elif desired_case == 'upper':
-        df[output] = df[input].str.upper()
-    elif desired_case == 'title':
-        df[output] = df[input].str.title()
-    elif desired_case == 'sentence':
-        df[output] = df[input].str.capitalize()
+    # If a string provided, convert to list
+    if isinstance(input, str):
+        input = [input]
+        output = [output]
+
+    # Loop through and apply for all columns
+    for input_column, output_column in zip(input, output):
+        if desired_case == 'lower':
+            df[output_column] = df[input_column].str.lower()
+        elif desired_case == 'upper':
+            df[output_column] = df[input_column].str.upper()
+        elif desired_case == 'title':
+            df[output_column] = df[input_column].str.title()
+        elif desired_case == 'sentence':
+            df[output_column] = df[input_column].str.capitalize()
 
     return df
 
 
-def data_type(df: _pd.DataFrame, input: str, output: str = None, parameters: dict = {}) -> _pd.DataFrame:
+def data_type(df: _pd.DataFrame, input: _Union[str, list], output: _Union[str, list] = None, parameters: dict = {}) -> _pd.DataFrame:
     """
-    Change the data type of the input
+    Change the data type of the input.
 
     ```
     wrangles:
@@ -58,34 +61,39 @@ def data_type(df: _pd.DataFrame, input: str, output: str = None, parameters: dic
     ```
     :param df: Input Dataframe
     :param input: Input column or list of columns to be operated on
-    :param output: (Optional) Output column or list of columns to save results to. If omitted, columns will be altered in place.
-    :param parameters: Dict of settings - desired data type
-    :return: Update Dataframe
+    :param output: (Optional) Output column or list of columns to save results to. Default - input columns will be replaced
+    :param type: Desired data type
     """
-    # TODO: enable list or string for input/output
-
     # If output is not specified, overwrite input columns in place
     if output is None: output = input
 
-    df[output] = df[input].astype(parameters['dataType'])
+    # If a string provided, convert to list
+    if isinstance(input, str):
+        input = [input]
+        output = [output]
+
+    # Loop through and apply for all columns
+    for input_column, output_column in zip(input, output):
+        df[output_column] = df[input_column].astype(parameters['dataType'])
+
     return df
 
 
-def to_json(df: _pd.DataFrame, input: str, output: str = None) -> _pd.DataFrame:
+def to_json(df: _pd.DataFrame, input: _Union[str, list], output: _Union[str, list] = None) -> _pd.DataFrame:
     """
-    Convert an object to a JSON representation
+    Convert an object to a JSON representation.
 
+    ```
+    wrangles:
+      - convert.to_json:
+          input: column
+          output: new column       # Optional
+    ```
     :param input: Name of the input column.
-    :param output: (Optional) Name of output column. If omitted, the input column will be replaced.
+    :param output: (Optional) Name or list of output column(s). Default - input columns will be replaced
     """
     # Set output column as input if not provided
     if output is None: output = input
 
-    output_list = []
-    for row in df[input].values.tolist():
-        output_list.append(_json.dumps(row))
-
-    df[output] = output_list
+    df[output] = [_json.dumps(row) for row in df[input].values.tolist()]
     return df
-
-
--- wrangles/pipeline_wrangles/create.py
+++ wrangles/pipeline_wrangles/create.py
@@ -9,15 +9,23 @@ from typing import Union as _Union
 
 def column(df: _pd.DataFrame, output: _Union[str, list], parameters: dict = {}) -> _pd.DataFrame:
     """
-    Create a column with a user defined value. Defaults to None (empty).
+    Create column(s) with a user defined value. Defaults to None (empty).
+
+    ```
+    wrangles:
+      - create.column:
+          output: new column
+          value: my value        # Optional
+    ```
 
     :param df: Input Dataframe
-    :param output: New column to be created
+    :param output: Name or list of names of new columns
+    :param value: (Optional) Value to add in the new column(s). If omitted, defaults to None.
     """
     # If a string provided, convert to list
     if isinstance(output, str): output = [output]
 
-    # Loop through and create uuid for all requested columns
+    # Loop through and create new columns
     for output_column in output:
         df[output_column] = parameters.get('value', None)
 
@@ -26,14 +34,36 @@ def column(df: _pd.DataFrame, output: _Union[str, list], parameters: dict = {}) 
 
 def guid(df: _pd.DataFrame, output: _Union[str, list]) -> _pd.DataFrame:
     """
-    Create a column with a GUID
+    Create column(s) with a GUID
+
+    ```
+    wrangles:
+      - create.guid:
+          output: new column
+    ```
+
+    :param df: Input Dataframe
+    :param output: Name or list of names of new columns
     """
     return uuid(df, output)
 
 
 def index(df: _pd.DataFrame, output: _Union[str, list], parameters: dict = {}) -> _pd.DataFrame:
     """
-    Create a new incremental index.
+    Create column(s) with an incremental index.
+
+    ```
+    wrangles:
+      - create.index:
+          output: new column
+          start: 1                  # Optional
+          step: 1                   # Optional
+
+    :param df: Input Dataframe
+    :param output: Name or list of names for new column(s)
+    :param start: (Optional; default 1) Starting number for the index
+    :param step: (Optional; default 1) Step between successive rows
+    ```
     """
     # Get start number if provided, default 1
     start = parameters.get('start', 1)
@@ -43,14 +73,23 @@ def index(df: _pd.DataFrame, output: _Union[str, list], parameters: dict = {}) -
 
     # Loop through and create incremental index
     for output_column in output:
-        df[output_column] = _np.arange(start, len(df) + start)
+        df[output_column] = _np.arange(start, len(df) * parameters.get('step', 1) + start, step=parameters.get('step', 1))
 
     return df
 
 
 def uuid(df: _pd.DataFrame, output: _Union[str, list]) -> _pd.DataFrame:
     """
-    Create a column with a UUID
+    Create column(s) with a UUID
+
+    ```
+    wrangles:
+      - create.uuid:
+          output: new column
+    ```
+
+    :param df: Input Dataframe
+    :param output: Name or list of names of new columns
     """
     # If a string provided, convert to list
     if isinstance(output, str): output = [output]
