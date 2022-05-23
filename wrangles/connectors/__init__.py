c751997367ea07c1ef09eae4b63f9d6c1885e0d1
Chris Ince <chris@wrangleworks.com>
2022-05-23 17:44:20 -0500

    Add notification connector

--- wrangles/connectors/__init__.py
+++ wrangles/connectors/__init__.py
@@ -7,3 +7,4 @@ from . import mssql
 from . import postgres
 from . import table
 from . import ssh
+from . import notification
---
+++ wrangles/connectors/notification.py
@@ -0,0 +1,29 @@
+import apprise
+
+
+_schema = {}
+
+
+def execute(url: str, title: str, body: str):
+    app_object = apprise.Apprise()
+    app_object.add(url)
+    app_object.notify(body, title)
+
+_schema['execute'] = """
+type: object
+description: Send a notification
+required:
+  - url
+  - title
+  - body
+properties:
+  url:
+    type: string
+    description: Apprise notification url. See https://github.com/caronc/apprise
+  title:
+    type: string
+    description: The title of the notification
+  body:
+    type: string
+    description: The body of the notification
+"""
