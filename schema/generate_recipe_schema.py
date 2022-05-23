964c717f4777cbd3f506c8137e636ce60474e28f
Chris Ince <chris@wrangleworks.com>
2022-05-23 16:17:50 -0500

    Significant updates

--- schema/generate_recipe_schema.py
+++ schema/generate_recipe_schema.py
@@ -3,9 +3,9 @@ import json
 import yaml
 
 schema = {
-    'read': [],
-    'wrangles': [],
-    'write': []
+    'read': {},
+    'wrangles': {},
+    'write': {}
 }
 
 # Load base recipe
@@ -18,25 +18,70 @@ for name in connectors:
     connector = getattr(wrangles.connectors, name)
     if '_schema' in dir(connector):
         if 'read' in connector._schema.keys():
-            schema_temp = {
-                'type': 'object',
-                'properties': {
-                    name: yaml.safe_load(connector._schema['read'])
-                }
-            }
-            schema['read'].append(schema_temp)
+            schema['read'][name] = yaml.safe_load(connector._schema['read'])
 
         if 'write' in connector._schema.keys():
-            schema_temp = {
-                'type': 'object',
-                'properties': {
-                    name: yaml.safe_load(connector._schema['write'])
-                }
-            }
-            schema['write'].append(schema_temp)
+            schema['write'][name] = yaml.safe_load(connector._schema['write'])
 
-recipe_schema['properties']['read']['oneOf'] = schema['read']
-recipe_schema['properties']['write']['items']['anyOf'] = schema['write']
+join = """
+type: object
+description: Join two data sources on key(s). Analogous to a join in SQL.
+required:
+  - how
+  - left_on
+  - right_on
+  - sources
+properties:
+  how:
+    type: string
+    description: Method of join
+    enum:
+      - left
+      - right
+      - outer
+      - inner
+      - cross
+  left_on:
+    type:
+      - string
+      - array
+    description: Key(s) to join on from first source
+  right_on:
+    type:
+      - string
+      - array
+    description: Key(s) to join on from second source
+  sources:
+    type: array
+    description: Two data sources to be joined
+    minItems: 2
+    maxItems: 2
+    items:
+      "$ref": "#/$defs/sources/read"
+"""
+schema['read']['join'] = yaml.safe_load(join)
+
+concatenate = """
+type: object
+description: Concatenate two data sets together. Analogous to a union in SQL.
+required:
+  - sources
+properties:
+  sources:
+    type: array
+    description: Two data sources to be joined
+    minItems: 2
+    items:
+      "$ref": "#/$defs/sources/read"
+  axis:
+    type: integer
+    description: 0 to stack vertically (default). 1 to stack horizontally.
+"""
+schema['read']['concatenate'] = yaml.safe_load(concatenate)
+
+
+recipe_schema['$defs']['sources']['read']['properties'] = schema['read']
+recipe_schema['properties']['write']['items']['properties'] = schema['write']
 
 
 
@@ -47,26 +92,14 @@ for name in pipeline_wrangles:
     if '_schema' in dir(pipeline_wrangle):
         for key, val in pipeline_wrangle._schema.items():
             if name == 'main':
-                schema_temp = {
-                    'type': 'object',
-                    'properties': {
-                        f'{key}': yaml.safe_load(val)
-                    }
-                }
-                schema['wrangles'].append(schema_temp)
+                schema['wrangles'][key] = yaml.safe_load(val)
             else:
-                schema_temp = {
-                    'type': 'object',
-                    'properties': {
-                        f'{name}.{key}': yaml.safe_load(val)
-                    }
-                }
-                schema['wrangles'].append(schema_temp)
+                schema['wrangles'][f'{name}.{key}'] = yaml.safe_load(val)
 
 
-recipe_schema['properties']['wrangles']['items']['anyOf'] = schema['wrangles']
+recipe_schema['properties']['wrangles']['items']['properties'] = schema['wrangles']
 
 
 # Write final schema
-with open('recipe_schema.json', 'w') as f:
+with open('recipes_schema.json', 'w') as f:
     json.dump(recipe_schema, f)
