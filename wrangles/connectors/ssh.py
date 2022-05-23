8d915e18c1d18f9797a46bb8524e6b4842ae21bd
Chris Ince <chris@wrangleworks.com>
2022-05-23 17:43:49 -0500

    Add schema

--- wrangles/connectors/ssh.py
+++ wrangles/connectors/ssh.py
@@ -4,8 +4,33 @@ Connector for SSH
 from fabric import Connection
 
 
+_schema = {}
+
 def execute(host: str, user: str, password: str, command: str) -> None:
     """
     Execute a command over ssh
     """
     Connection(host, user=user, connect_kwargs={'password': password}).run(command)
+
+_schema['execute'] = """
+type: object
+description: Issue commands over SSH
+required:
+  - host
+  - user
+  - password
+  - command
+properties:
+  host:
+    type: string
+    description: Domain or IP of the host
+  user:
+    type: string
+    description: The user to connect as
+  password:
+    type: string
+    description: Password for the user
+  command:
+    type: string
+    description: Command to send
+"""
