a38572d91fe1fc92b4f4de64ae78fea4f0de4f90
Chris Ince <chris@wrangleworks.com>
2022-05-24 09:24:34 -0500

    Add trim wrangle

--- wrangles/pipeline_wrangles/format.py
+++ wrangles/pipeline_wrangles/format.py
@@ -5,6 +5,9 @@ import pandas as _pd
 from .. import format as _format
 
 
+_schema = {}
+
+
 def price_breaks(df: _pd.DataFrame, input: list, categoryLabel: str, valueLabel: str) -> _pd.DataFrame:
     """
     Rearrange price breaks
@@ -22,3 +25,38 @@ def remove_duplicates(df: _pd.DataFrame, input: str, output: str) -> _pd.DataFra
             output_list.append(row)
     df[output] = output_list
     return df
+
+
+def trim(df: _pd.DataFrame, input: str, output: str = None) -> _pd.DataFrame:
+    """
+    Remove excess whitespace at the start and end of text
+    """
+    if output is None: output = input
+
+    # If a string provided, convert to list
+    if isinstance(output, str): output = [output]
+
+    # Loop through and create uuid for all requested columns
+    for input_column, output_column in zip(input, output):
+        df[output_column] = df[input_column].str.strip()
+
+    return df
+
+_schema['trim'] = """
+type: object
+description: Remove excess whitespace at the start and end of text
+additionalProperties: false
+required:
+  - input
+properties:
+  input:
+    type:
+      - array
+      - string
+    description: Name of the input column
+  output:
+    type:
+      - array
+      - string
+    description: Name of the output column
+"""
