c6ca98953744e32d3bd8705223b47ec8f7a97ba9
Chris Ince <chris@wrangleworks.com>
2022-05-26 16:59:03 -0500

    Add pricefx connector

--- wrangles/connectors/__init__.py
+++ wrangles/connectors/__init__.py
@@ -5,6 +5,7 @@ Connectors to read/write from external systems
 from . import file
 from . import mssql
 from . import postgres
+from . import pricefx
 from . import table
 from . import ssh
 from . import notification
