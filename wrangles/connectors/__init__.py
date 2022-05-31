8d12414a8796e29070b16bf7f32e719f52a73907
Chris Ince <chris@wrangleworks.com>
2022-05-31 16:31:42 -0500

    Update imports

--- wrangles/connectors/__init__.py
+++ wrangles/connectors/__init__.py
@@ -3,9 +3,11 @@ Connectors to read/write from external systems
 """
 
 from . import file
+from . import http
 from . import mssql
+from . import mysql
+from . import notification
 from . import postgres
 from . import pricefx
 from . import table
 from . import ssh
-from . import notification
