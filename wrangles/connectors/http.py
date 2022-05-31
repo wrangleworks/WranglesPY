26f62b5881e3ac74c9261095e417ca0a5be25447
Chris Ince <chris@wrangleworks.com>
2022-05-31 17:13:16 -0500

    Add read function

--- wrangles/connectors/http.py
+++ wrangles/connectors/http.py
@@ -2,6 +2,7 @@
 Connector to make http(s) requests
 """
 import requests as _requests
+import pandas as _pd
 
 
 _schema = {}
@@ -36,4 +37,42 @@ properties:
 """
 
 
-# TODO: read and write
+def read(url: str, method: str = "GET", headers: dict = None, json_key: str = None) -> None:
+    response = _requests.request(method=method, url=url, headers=headers)
+    response_json = response.json()
+    if json_key:
+      for element in json_key.split('.'):
+        response_json = response_json[element]
+    df = _pd.json_normalize(response_json, max_level=0)
+    return df
+
+_schema['read'] = """
+type: object
+description: Get data from a HTTP endpoint.
+required:
+  - url
+properties:
+  url:
+    type: string
+    description: The URL to make the request to
+  method:
+    type: string
+    description: The http method to use. Default GET.
+    enum:
+      - GET
+      - POST
+      - PUT
+      - PATCH
+      - DELETE
+      - HEAD
+      - OPTIONS
+  headers:
+    type: object
+    description: Headers to pass as part of the request
+  json_key:
+    type: string
+    description: Select sub-elements from the response JSON. Multiple levels can be specified with e.g. key1.key2.key3
+"""
+
+
+# TODO: write
