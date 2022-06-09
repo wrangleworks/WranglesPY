85503c57961d6b05a454d564989d85cbe6fef4cc
cerdamario13 <mario@wrangleworks.com>
2022-06-09 13:38:43 -0500

    Added more meaningful error messages

--- wrangles/extract.py
+++ wrangles/extract.py
@@ -112,8 +112,15 @@ def custom(input: _Union[str, list], model_id: str) -> list:
     url = f'{_config.api_host}/wrangles/extract/custom'
     params = {'responseFormat': 'array', 'model_id': model_id}
     model_properties = _data.model(model_id)
+    # If model_id format is correct but no mode_id exists
+    if model_properties.get('message', None) == 'error': raise ValueError('Incorrect model_id.\nmodel_id may be wrong or does not exists')
     batch_size = model_properties['batch_size'] or 10000
-
+    
+    # Using model_id in wrong function
+    purpose = model_properties['purpose']
+    if purpose != 'extract':
+        raise ValueError(f'Using {purpose} model_id in an extract function.')
+    
     results = _batching.batch_api_calls(url, params, json_data, batch_size)
 
     if isinstance(input, str): results = results[0]
