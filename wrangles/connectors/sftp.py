9ecd3c636b4f1edcca02a26a969daf961a92ac43
Chris Ince <chris@ince.live>
2022-06-09 19:16:53 -0500

    Add write function

--- wrangles/connectors/sftp.py
+++ wrangles/connectors/sftp.py
@@ -100,3 +100,97 @@ properties:
       - columns
       - values
 """
+
+
+def write(df, host: str, user: str, password: str, file: str, port: int = 22, columns: list = None, **kwargs) -> None:
+    """
+    Read files to an SFTP server
+
+    Supports Excel (.xlsx, .xls), CSV (.csv, .txt) and JSON (.json) files.
+    JSON and CSV may also be gzipped (.csv.gz, .txt.gz, .json.gz) and will be compressed.
+
+    >>> from wrangles.connectors import sftp
+    >>> df = sftp.write(df, host='sftp.domain', user='user', password='password', file='myfile.csv')
+
+    :param df: Dataframe to be written to a file
+    :param host: The domain or IP of the SFTP server
+    :param user: The user to connect as
+    :param password: The password for the user
+    :param file: The filename including path on the remote server
+    """
+    _logging.info(f": Exporting data via SFTP :: {host}")
+
+    # If the user specifies only certain columns, only include those
+    if columns is not None: df = df[columns]
+
+    # Get the file from the SFTP server
+    tempFile = _io.BytesIO()
+
+    # Write appropriate file
+    if file.split('.')[-1] in ['xlsx', 'xls']:
+        # Write an Excel file
+        # Default to not including index if user hasn't explicitly requested it
+        if 'index' not in kwargs.keys(): kwargs['index'] = False
+        df.to_excel(tempFile, **kwargs)
+
+    elif file.split('.')[-1] in ['csv', 'txt'] or '.'.join(file.split('.')[-2:]) in ['csv.gz', 'txt.gz']:
+        # Write a CSV file
+        # Default to not including index if user hasn't explicitly requested it
+        if 'index' not in kwargs.keys(): kwargs['index'] = False
+        df.to_csv(tempFile, **kwargs)
+
+    elif file.split('.')[-1] in ['json'] or '.'.join(file.split('.')[-2:]) in ['json.gz']:
+        # Write a JSON file
+        # If user doesn't explicitly set orient, assume records - this seems a better default
+        if 'orient' not in kwargs.keys(): kwargs['orient'] = 'records'
+        df.to_json(tempFile, **kwargs)
+        
+    elif file.split('.')[-1] in ['jsonl'] or '.'.join(file.split('.')[-2:]) in ['jsonl.gz']:
+        # Write a line delimited JSONL file
+        # Set lines to true 
+        kwargs['lines'] = True
+        # Only records orientation is supported for JSONL
+        kwargs['orient'] = 'records'
+        df.to_json(tempFile, **kwargs)
+
+    tempFile.seek(0)  # Reset file to start
+    _fabric.Connection(host, user=user, port=port, connect_kwargs={'password': password}).put(tempFile, remote=file)
+
+
+_schema['write'] = """
+type: object
+description: Export a file to an SFTP server
+required:
+  - host
+  - user
+  - password
+  - file
+properties:
+  host:
+    type: string
+    description: The domain or IP of the SFTP server
+  user:
+    type: string
+    description: The user to connect as
+  password:
+    type: string
+    description: The password for the user
+  file:
+    type: string
+    description: The filename including path on the remote server
+  columns:
+    type: array
+    description: Choose only a subset of the columns to export
+  sheet_name:
+    type: string
+    description: Used for Excel files. Specify the sheet to create.
+  orient:
+    type: string
+    description: Used for JSON files. Specifies the output arrangement
+    enum:
+      - split
+      - records
+      - index
+      - columns
+      - values
+"""
