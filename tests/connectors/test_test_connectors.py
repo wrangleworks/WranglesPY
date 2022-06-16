2e0d1281c8769298a8db29547bc652e7dc511655
cerdamario13 <mario@wrangleworks.com>
2022-06-16 16:36:04 -0500

    more tests

---
+++ tests/connectors/test_test_connectors.py
@@ -0,0 +1,51 @@
+import wrangles
+
+
+def test_data_generation1():
+    recipe = """
+    read:
+        - test:
+            rows: 5
+            values:
+                fixed value: example string
+                code: <code(10)>
+                boolean: <boolean>
+                number: <number(2.718-3.141)>
+                int: <int(0-100)>
+                char: <char>
+                word: <word>
+                sentence: <sentence>
+                list:
+                    - apple
+                    - strawberry
+                    - pear
+                    - banana
+    """
+    df = wrangles.pipeline.run(recipe)
+    assert df.columns.tolist() == ['fixed value', 'code', 'boolean', 'number', 'int', 'char', 'word', 'sentence', 'list']
+    
+# Not valid int values
+def test_data_generation_2():
+    recipe = """
+    read:
+        - test:
+            rows: 5
+            values:
+                int: <int>
+                char: <char>
+                word: <word>
+                sentence: <NotFound>
+                number: <number(2-13.13)>
+                code: <code>
+                dict: 
+                    hello: good bye
+                list:
+                    - apple
+                    - strawberry
+                    - pear
+                    - banana
+    """
+    df = wrangles.pipeline.run(recipe)
+    assert df.columns.tolist() == ['int', 'char', 'word', 'sentence', 'number', 'code', 'dict', 'list']
+    
+  
