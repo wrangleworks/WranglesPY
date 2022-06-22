d2d484b6688f09cf11de44b7098928570f0fa3aa
Chris Ince <chris@wrangleworks.com>
2022-06-22 10:39:23 -0500

    Add ability to specify a file of custom functions

--- main.py
+++ main.py
@@ -1,5 +1,19 @@
 import wrangles
 import os
+import importlib.util
+import sys
 
 
-wrangles.pipeline.run(os.environ["WRANGLES_RECIPE"])
+# If the user has provided a file of custom functions, add those
+functions = []
+if os.environ.get("WRANGLES_CUSTOM_FUNCTIONS"):
+    file_path = os.environ.get("WRANGLES_CUSTOM_FUNCTIONS")
+    spec = importlib.util.spec_from_file_location('custom_functions', file_path)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules['custom_functions'] = module
+    spec.loader.exec_module(module)
+
+    functions = [getattr(module, method) for method in dir(module) if not method.startswith('_')]
+
+
+wrangles.pipeline.run(os.environ["WRANGLES_RECIPE"], functions=functions)
