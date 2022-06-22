b3173f10edb3b6562da67bc7d2413ba3067c2a99
cerdamario13 <mario@wrangleworks.com>
2022-06-22 17:15:58 -0500

    mongodb test

--- tests/connectors/test_mongodb_connector.py
+++ tests/connectors/test_mongodb_connector.py
@@ -2,21 +2,26 @@ import wrangles
 import pymongo
 import pandas as pd
 
-# from wrangles.connectors.mongodb import read, write
-# def test_read(mocker):
-#     m2 = mocker.patch("pymongo.MongoClient")
-#     m2.return_value = {"db": {"col": "collection"}}
-#     data = [{'name': 'Data1'}, {'name': 'Data2'}]
-#     m = mocker.patch("pymongo.collection.Collection.find")
-#     m.return_value = data
-#     config = {
-#         'user': 'us',
-#         'password': 'pass',
-#         'database': 'db',
-#         'collection': 'col',
-#         'host': 'host',
-#         'query': '{"name": "Data1"}',
-#         'projection': '{}'
-#     }
-#     test = read(**config)
-#     print()
+class temp_col():
+    def find(query, projection):
+        return {'name': 'Data1'}
+    
+        
+
+from wrangles.connectors.mongodb import read, write
+
+# return an empty query
+def test_read(mocker):
+    m2 = mocker.patch("pymongo.MongoClient")
+    m2.return_value = {"db": {"col": temp_col}}
+    config = {
+        'user': 'us',
+        'password': 'pass',
+        'database': 'db',
+        'collection': 'col',
+        'host': 'host',
+        'query': '{"name": "Data1"}',
+        'projection': '{}'
+    }
+    test = read(**config)
+    assert test[0][0] == 'name'
