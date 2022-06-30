1193400744f73321cdbce9bfa1b426a83ce0d786
Chris Ince <chris@wrangleworks.com>
2022-06-30 10:33:11 -0500

    Update error message

--- tests/pipelines/wrangles/test_main.py
+++ tests/pipelines/wrangles/test_main.py
@@ -80,7 +80,7 @@ def test_classify_4():
     """
     with pytest.raises(ValueError) as info:
         raise wrangles.recipe.run(recipe, dataframe=data)
-    assert info.typename == 'ValueError' and info.value.args[0] == 'Incorrect model_id. May be missing "${ }" around value'
+    assert info.typename == 'ValueError' and info.value.args[0] == 'Incorrect or missing values in model_id. Check format is XXXXXXXX-XXXX-XXXX'
     
 #
 # Filter
