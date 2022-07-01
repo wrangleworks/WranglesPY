4aa484e172754c970137cc41b1458b24fcbc3843
Chris Ince <chris@wrangleworks.com>
2022-07-01 15:28:58 -0500

    Add ability to train standardize models

--- wrangles/train.py
+++ wrangles/train.py
@@ -42,12 +42,36 @@ class train():
         if isinstance(training_data, list):
             check_index = [training_data.index(x) for x in training_data if len(x) != 2]
             if len(check_index) != 0:
-                raise ValueError(f'Training_data list must contain a list of two elements. Check element(s) {check_index} in training_list.\nFormat:\nFirst element is "Entinty to Find"\nSecond Element is "Variation", If no variation, use \'\'\n'
-                "Example:[['Television', 'TV]]")
+                raise ValueError(f'Training_data list must contain a list of two elements. Check element(s) {check_index} in training_list.\nFormat:\nFirst element is "Entity to Find"\nSecond Element is "Variation", If no variation, use \'\'\n'
+                "Example:[['Television', 'TV']]")
             # checking the first element in training list
             if training_data[0] != ['Entity to Find', 'Variation (Optional)']:
                 training_data = [['Entity to Find', 'Variation (Optional)']] + training_data
         
         # Validate input here
         response = _requests.post(f'{_config.api_host}/data/user/model/train', params={'type':'extract', 'name': name}, headers={'Authorization': f'Bearer {_auth.get_access_token()}'}, json=training_data)
+        return response
+
+    def standardize(training_data: list, name: str):
+        """
+        Train a standardize model. This can standardize text to a desired format.
+        
+        :param training_data: paired list of entities to find and replace.
+        :param name: The name of the new model.
+        """
+        # If input is a list, check to make sure that all sublists are length of 2
+        if isinstance(training_data, list):
+            check_index = [training_data.index(x) for x in training_data if len(x) != 2]
+            if len(check_index) != 0:
+                raise ValueError(f'Training_data list must contain a list of two elements. Check element(s) {check_index} in training_list.\nFormat:\nFirst element is "Entity to Find"\nSecond Element is "Variation", If no variation, use \'\'\n'
+                "Example:[['USA', 'United States of America']]")
+                
+            # checking the first element in training list
+            if training_data[0] != ['Find', 'Replace']:
+                training_data = [['Find', 'Replace']] + training_data
+        else:
+            raise ValueError('A list is expected for training_data')
+        
+        # Validate input here
+        response = _requests.post(f'{_config.api_host}/data/user/model/train', params={'type':'standardize', 'name': name}, headers={'Authorization': f'Bearer {_auth.get_access_token()}'}, json=training_data)
         return response
