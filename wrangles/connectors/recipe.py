30c12b86aa31a20c4c0ab3c58b0f9beba87a3dd9
Chris Ince <chris@wrangleworks.com>
2022-07-12 11:14:07 -0500

    Add write

--- wrangles/connectors/recipe.py
+++ wrangles/connectors/recipe.py
@@ -4,6 +4,7 @@ Connector to run a recipe.
 Run a recipe, from a recipe! Recipe-ception.
 """
 from .. import recipe as _recipe
+import pandas as _pd
 
 
 _schema = {}
@@ -62,4 +63,39 @@ properties:
   variables:
     type: object
     description: A dictionary of variables to pass to the recipe
+"""
+
+
+def write(df: _pd.DataFrame, name: str, columns: list = None, variables: dict = {}) -> None:
+    """
+    Read a recipe.
+
+    >>> from wrangles.connectors import recipe
+    >>> recipe.write(dataframe=df, name='recipe.wrgl.yml')
+
+    :param df: Dataframe to start the recipe with
+    :param name: Name of the recipe to run
+    :param variables: (Optional) A dictionary of custom variables to override placeholders in the recipe. Variables can be indicated as ${MY_VARIABLE}. Variables can also be overwritten by Environment Variables.
+    """
+    # Select only specific columns if user requests them
+    if columns is not None: df = df[columns]
+
+    _recipe.run(name, dataframe=df, variables=variables)
+
+
+_schema['write'] = """
+type: object
+description: Run a recipe, from a recipe! Recipe-ception. This will trigger a new recipe with the contents of the current recipe.
+required:
+  - name
+properties:
+  name:
+    type: string
+    description: The name of the recipe to read from
+  variables:
+    type: object
+    description: A dictionary of variables to pass to the recipe
+  columns:
+    type: array
+    description: (Optional) A list of the columns to pass to the recipe. If omitted, all columns will be included.
 """
