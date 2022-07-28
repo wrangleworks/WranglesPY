288ff0340e95db5eb6c62d04aad0d4c22496ecba
Chris Ince <chris@wrangleworks.com>
2022-07-28 14:14:28 -0500

    Enable passing passing private key in recipe

--- wrangles/connectors/ssh.py
+++ wrangles/connectors/ssh.py
@@ -3,10 +3,12 @@ Connector for SSH
 """
 from fabric import Connection as _Connection
 from typing import Union as _Union
+from paramiko import RSAKey as _RSAKey
+from io import StringIO as _StringIO
 
 _schema = {}
 
-def run(host: str, user: str, command: _Union[str, list], password: str = None, key_filename: str = None) -> None:
+def run(host: str, user: str, command: _Union[str, list], password: str = None, key_filename: str = None, private_key: str = None) -> None:
     """
     Execute a command over ssh
 
@@ -14,6 +16,7 @@ def run(host: str, user: str, command: _Union[str, list], password: str = None, 
     :param user: User to connect as
     :param password: Password for the user
     :param key_filename: File that contains the private key
+    :param private_key: Provide an RSA Private Key as a string
     :param command: Command or list of commands to execute. When providing a list, note that all commands are executed in isolation, i.e. cd /dir in a prior command will not affect the directory for later commands.  
     """
     # If user has passed a single command, convert to a list of one
@@ -23,6 +26,9 @@ def run(host: str, user: str, command: _Union[str, list], password: str = None, 
         connect_kwargs = {'password': password}
     elif key_filename is not None:
         connect_kwargs = {'key_filename': key_filename}
+    elif private_key is not None:
+        pkey = _RSAKey.from_private_key(_StringIO(private_key))
+        connect_kwargs = {'pkey': pkey}
     else:
         raise ValueError('A password or private key is required for the SSH connection')
 
@@ -49,6 +55,12 @@ properties:
   password:
     type: string
     description: Password for the user
+  key_filename:
+    type: string
+    description: Path to a file that contains the private key
+  private_key:
+    type: string
+    description: Provide an RSA Private Key as a string
   command:
     type:
       - string
