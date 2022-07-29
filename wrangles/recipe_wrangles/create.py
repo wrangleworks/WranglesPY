1df5c42e6e3aa4bcd9b4d9fd50095729226d11fd
cerdamario13 <mario@wrangleworks.com>
2022-07-29 15:23:07 -0500

    create.bins for values

--- wrangles/recipe_wrangles/create.py
+++ wrangles/recipe_wrangles/create.py
@@ -5,6 +5,7 @@ import pandas as _pd
 import uuid as _uuid
 import numpy as _np
 from typing import Union as _Union
+import math
 
 
 def column(df: _pd.DataFrame, output: _Union[str, list], value = None) -> _pd.DataFrame:
@@ -103,3 +104,55 @@ def uuid(df: _pd.DataFrame, output: _Union[str, list]) -> _pd.DataFrame:
         df[output_column] = [_uuid.uuid4() for _ in range(len(df.index))]
 
     return df
+    
+    
+def bins(df: _pd.DataFrame, input: _Union[str, list], output: _Union[str, list], bins: _Union[int, list], labels: _Union[str, list] = None, **kwargs):
+    """
+    type: object
+    description: Creates a column that segment and sort data values into bins
+    additionalProperties: false
+    required:
+      - input
+      - output
+      - bins
+    properties:
+      input:
+        type:
+          - array
+        description: Name of input column
+      output:
+        type:
+          - array
+        description: Namen of new column
+      bins:
+        type:
+          - int
+          - array
+        description: Defines the number of equal-width bins in the range
+      labels:
+        type:
+          - str
+          - array
+        description: Labels for the returned bins
+    """
+    
+    # Dealing with positive infinity. At end of bins list
+    if isinstance(bins, list):
+        if bins[-1] == '+':
+            bins[-1] = math.inf
+        
+        # Dealing with negative infinity. At start of bins list
+        if bins[0] == '-':
+            bins[0] = -math.inf
+    
+    
+    df[output] = _pd.cut(
+        x=df[input],
+        bins=bins,
+        labels=labels,
+        **kwargs
+    )
+    print(df[['IMAP_Value', 'Pricing']].head(10).to_markdown(index=False))
+    
+    return df
+  
