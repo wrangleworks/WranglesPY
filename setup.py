af7ed8a08e0c4192415f391b51cec61f7358aed0
Chris Ince <chris@wrangleworks.com>
2022-08-05 17:27:54 -0500

    Add license info

--- setup.py
+++ setup.py
@@ -18,6 +18,13 @@ setup(
     description = 'Wrangle your data into shape with machine learning',
     long_description = long_description,
     long_description_content_type = 'text/markdown',
+    license_files = ('LICENSE.txt',),
+    license = 'Apache License 2.0',
+    classifiers = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: Apache Software License',
+        'Operating System :: OS Independent'
+    ],
     version = '0.5',
     url = 'https://wrangles.io',
     author = 'WrangleWorks',
