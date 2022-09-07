a588a603344fe6587006dccb8c3e143efb2b4ffa
cerdamario13 <mario@wrangleworks.com>
2022-09-07 15:11:30 -0500

    recipe connector test

--- tests/connectors/test_recipe.py
+++ tests/connectors/test_recipe.py
@@ -11,8 +11,49 @@ def test_read_recipe_connector():
           variables:
             inputFile: 'tests/samples/data.csv'
     """
-    # variables = {
-    #     'inputFile': 'tests/samples/data.csv'
-    # }
     df = wrangles.recipe.run(recipe)
     assert df['Find2'].iloc[0] == 'brg'
+    
+    
+# Recipe as a connector
+from wrangles.connectors.recipe import read, run
+def test_read_recipe_connector_2():
+  recipe = """
+  read:
+      - recipe:
+          name: 'tests/samples/recipe_sample.wrgl.yaml'
+          variables:
+            inputFile: 'tests/samples/data.csv'
+  """
+  df = read(recipe)
+  assert df['Find2'].iloc[0] == 'brg'
+  
+# writing
+def test_write_recipe_connector():
+  recipe = """
+  read:
+      - recipe:
+          name: 'tests/samples/recipe_sample.wrgl.yaml'
+          variables:
+            inputFile: 'tests/samples/data.csv'
+  write:
+      - recipe:
+          name: 'tests/samples/recipe_sample.wrgl.yaml'
+          variables:
+            inputFile: 'tests/samples/data.csv'
+  """
+  df = read(recipe)
+  assert df['Find2'].iloc[0] == 'brg'
+
+
+# Running recipe
+def test_run_recipe_connector():
+  recipe = """
+  run:
+    on_start:
+      - recipe:
+          name: 'tests/samples/recipe_sample.wrgl.yaml'
+          variables:
+            inputFile: 'tests/samples/data.csv'
+  """
+  assert run(recipe) == None
