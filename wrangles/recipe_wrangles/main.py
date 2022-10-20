38d0d1d2b83ff4f62e7de33495298c8ec96507a2
cerdamario13 <mario@wrangleworks.com>
2022-10-20 10:43:57 -0500

    sql to json uses to/from json functions

--- wrangles/recipe_wrangles/main.py
+++ wrangles/recipe_wrangles/main.py
@@ -8,6 +8,8 @@ from ..standardize import standardize as _standardize
 from ..translate import translate as _translate
 from .. import extract as _extract
 from .. import recipe as _recipe
+from .convert import to_json as _to_json
+from .convert import from_json as _from_json
 
 import numexpr as _ne
 import pandas as _pd
@@ -411,19 +413,21 @@ def sql(df: _pd.DataFrame, command: str) -> _pd.DataFrame:
     
     # List of columns changed
     cols_changed = []
-    # If column contains objects, then convert to json
     for cols in df.columns:
-                    
+        count = 0        
         for row in df[cols]:
-            
+            # If row contains objects, then convert to json
             if isinstance(row, dict):
                 # Check if there is an object in the column and record column name to convert to json
                 cols_changed.append(cols)
                 break
-        
-        # If the column is in cols_changed then convert to json
+            # Only check the first 10 rows of a column
+            count += 1
+            if count > 10: break
+            
         if cols in cols_changed:
-            df[cols] = [_json.dumps(rows) for rows in df[cols].values.tolist()]
+            # If the column is in cols_changed then convert to json
+            _to_json(df=df, input=cols)
     
     df.to_sql('df', db, if_exists='replace', index = False, method='multi', chunksize=1000)
     
@@ -436,7 +440,7 @@ def sql(df: _pd.DataFrame, command: str) -> _pd.DataFrame:
         
         if new_cols in cols_changed:
             # If the column is in cols changed, then change back to an object
-            df[new_cols] = [_json.loads(x) for x in df[new_cols].values.tolist()]
+            _from_json(df=df, input=new_cols)
     
     return df
 
