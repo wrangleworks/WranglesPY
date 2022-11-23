a1dbf43a26c48e93fb097d7cf3da20477a3d75c3
Thomas <thomas@wrangleworks.com>
2022-11-23 17:06:22 -0600

    Add read training data

--- wrangles/connectors/train.py
+++ wrangles/connectors/train.py
@@ -1,9 +1,23 @@
 import logging as _logging
 import pandas as _pd
 from ..train import train as _train
-
+from .. import data as _data
 
 class classify():
+    def read(model_id: str) -> _pd.DataFrame:
+        """
+        """
+        tmp_data = _data.model_data(model_id, 'classify')
+
+        if len(tmp_data[0]) == 2:
+            columns = ['Example', 'Category']
+        elif len(tmp_data[0]) == 3:
+            columns = ['Example', 'Category', 'Notes']
+        else:
+            columns = None
+
+        return _pd.DataFrame(tmp_data, columns=columns)
+
     def write(df: _pd.DataFrame, columns: list = None, name: str = None, model_id: str = None) -> None:
         """
         Train a new or existing classify wrangle
@@ -20,6 +34,20 @@ class classify():
 
 
 class extract():
+    def read(model_id: str) -> _pd.DataFrame:
+        """
+        """
+        tmp_data = _data.model_data(model_id, 'extract')
+
+        if len(tmp_data[0]) == 2:
+            columns = ['Entity to Find', 'Variation (Optional)']
+        elif len(tmp_data[0]) == 3:
+            columns = ['Entity to Find', 'Variation (Optional)', 'Notes']
+        else:
+            columns = None
+
+        return _pd.DataFrame(tmp_data, columns=columns)
+
     def write(df: _pd.DataFrame, columns: list = None, name: str = None, model_id: str = None) -> None:
         """
         Train a new or existing extract wrangle
@@ -36,6 +64,20 @@ class extract():
 
 
 class standardize():
+    def read(model_id: str) -> _pd.DataFrame:
+        """
+        """
+        tmp_data = _data.model_data(model_id, 'standardize')
+
+        if len(tmp_data[0]) == 2:
+            columns = ['Find', 'Replace']
+        elif len(tmp_data[0]) == 3:
+            columns = ['Find', 'Replace', 'Notes']
+        else:
+            columns = None
+
+        return _pd.DataFrame(tmp_data, columns=columns)
+
     def write(df: _pd.DataFrame, columns: list = None, name: str = None, model_id: str = None) -> None:
         """
         Train a new or existing standardize wrangle
