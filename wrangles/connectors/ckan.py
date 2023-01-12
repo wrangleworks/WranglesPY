fc58329e65d0f1be84c6ea79a3df0117cc843a20
Chris Ince <chris@wrangleworks.com>
2023-01-12 16:48:29 -0600

    First draft write function

--- wrangles/connectors/ckan.py
+++ wrangles/connectors/ckan.py
@@ -61,3 +61,51 @@ properties:
     type: string
     description: API Key for the CKAN site.
 """
+
+def write(df: _pd.DataFrame, host: str, dataset: str, file: str, api_key: str = None, **kwargs):
+    """
+    Write a file in AWS S3
+    
+    :param df: Dataframe to be exported
+    :param bucket: The name of the bucket where file will be written
+    :param key: The name of the key to download from
+    :param access_key: S3 access key
+    :param secret_access_key: S3 secret access key
+    :param kwargs: (Optional) Named arguments to pass to respective pandas write a file function.
+    """      
+    memory_file = _BytesIO()
+    _file.write(df, name=file, file_object=memory_file, **kwargs)
+    memory_file.seek(0, 0)
+    _logging.info(f": Writing File :: {host} / {dataset} / {file}")
+
+    response = _requests.post(
+        url = f"{host}/api/action/resource_create",
+        data={
+            "package_id": dataset,
+            "name": file
+        },
+        headers={"Authorization": api_key},
+        files=[('upload', memory_file.read())]
+    )
+    print()
+    
+_schema['write'] = """
+type: object
+description: write files to AWS S3
+required:
+  - bucket
+  - key
+properties:
+  bucket:
+    type: string
+    description: The name of the bucket where file will be written
+  key:
+    type: string
+    description: The name of the key of the file written
+  access_key:
+    type: string
+    description: S3 access key
+  secret_access_key:
+    type: string
+    description: S3 secret access key
+"""
