ee4be63b357b4d5b1d9264c71c3d8907e013debf
Chris Ince <chris@wrangleworks.com>
2023-01-27 14:41:46 -0600

    Add write (create and update)

--- wrangles/connectors/ckan.py
+++ wrangles/connectors/ckan.py
@@ -7,6 +7,26 @@ from . import file as _file
 
 _schema = {}
 
+
+def _get_packages_in_dataset(host, dataset, api_key):
+    response = _requests.get(
+        url = f"{host}/api/3/action/package_show?id={dataset}",
+        headers={'Authorization': api_key}
+    )
+    
+    if response.status_code == 403:
+        raise RuntimeError("Access Denied to the CKAN Server. If this is a private resource, you must provide an API_KEY.")
+    
+    if response.status_code != 200:
+        raise ValueError("Unable to find dataset on CKAN server. Check host and dataset name.")
+    
+    results = {}
+    for resource in response.json()["result"]["resources"]:
+        results[resource["name"]] = resource
+    
+    return results
+
+
 def read(host: str, dataset: str, file: str, api_key: str = None, **kwargs) -> _pd.DataFrame:
     """
     Read data from CKAN
@@ -19,22 +39,13 @@ def read(host: str, dataset: str, file: str, api_key: str = None, **kwargs) -> _
     """
     _logging.info(f": Importing Data :: {host}/{dataset}/{file}")
     
-    response = _requests.get(
-        url = f"{host}/api/3/action/package_show?id={dataset}",
-        headers={'Authorization': api_key}
-    )
+    packages = _get_packages_in_dataset(host, dataset, api_key)
+
+    if file not in packages.keys():
+        raise ValueError('File not found in dataset')
     
-    # Get download url from dataset and file names
-    download_url = None
-    for resource in response.json()["result"]["resources"]:
-        if resource["name"] == file:
-            download_url = resource["url"]
-
-    if download_url == None:
-        raise ValueError('Dataset Not Found')
-
     # Download the requested data
-    response = _requests.get(download_url, headers={'Authorization': api_key})
+    response = _requests.get(packages[file]["url"], headers={'Authorization': api_key})
     file_io = _BytesIO(response.content)
     df = _file.read(file, file_object=file_io, **kwargs)    
  
@@ -56,7 +67,7 @@ properties:
     description: The name of the dataset. This should be the url version e.g. my-dataset
   file:
     type: string
-    description: The name of the specific file within the dataset.
+    description: The name of the specific file within the dataset. e.g. example.csv
   api_key:
     type: string
     description: API Key for the CKAN site.
@@ -64,48 +75,64 @@ properties:
 
 def write(df: _pd.DataFrame, host: str, dataset: str, file: str, api_key: str = None, **kwargs):
     """
-    Write a file in AWS S3
+    Write a file to a dataset in CKAN
     
     :param df: Dataframe to be exported
-    :param bucket: The name of the bucket where file will be written
-    :param key: The name of the key to download from
-    :param access_key: S3 access key
-    :param secret_access_key: S3 secret access key
+    :param host: The host name of the CKAN site. e.g. data.example.com
+    :param dataset: The name of the dataset. This should be the url version e.g. my-dataset
+    :param file: The name of the specific file within the dataset. e.g. example.csv
+    :param api_key: API Key for the CKAN site.
     :param kwargs: (Optional) Named arguments to pass to respective pandas write a file function.
-    """      
+    """
     memory_file = _BytesIO()
     _file.write(df, name=file, file_object=memory_file, **kwargs)
-    memory_file.seek(0, 0)
+    # memory_file.seek(0, 0)
     _logging.info(f": Writing File :: {host} / {dataset} / {file}")
 
-    response = _requests.post(
-        url = f"{host}/api/action/resource_create",
-        data={
-            "package_id": dataset,
-            "name": file
-        },
-        headers={"Authorization": api_key},
-        files=[('upload', memory_file.read())]
-    )
-    print()
+    packages = _get_packages_in_dataset(host, dataset, api_key)
+
+    if file in packages.keys():
+        response = _requests.post(
+            url = f"{host}/api/action/resource_update",
+            data = {"id": packages[file]["id"]},
+            headers = {"Authorization": api_key},
+            files = {'upload': (file, memory_file.getvalue())}
+        )
+    else:
+        response = _requests.post(
+            url = f"{host}/api/action/resource_create",
+            data={
+                "package_id": dataset,
+                "name": file
+            },
+            headers={"Authorization": api_key},
+            files={'upload': (file, memory_file.getvalue())}
+        )
+
+    if response.status_code == 403:
+        raise RuntimeError("Access Denied to the CKAN Server. Check your API_KEY.")
+    elif response.status_code != 200:
+        raise RuntimeError("Upload to CKAN Failed")
     
 _schema['write'] = """
 type: object
-description: write files to AWS S3
+description: Write a file to a dataset in CKAN
 required:
-  - bucket
-  - key
+  - host
+  - api_key
+  - dataset
+  - file
 properties:
-  bucket:
+  host:
     type: string
-    description: The name of the bucket where file will be written
-  key:
+    description: The host name of the CKAN site. e.g. data.example.com
+  api_key:
     type: string
-    description: The name of the key of the file written
-  access_key:
+    description: API Key for the CKAN site.
+  dataset:
     type: string
-    description: S3 access key
-  secret_access_key:
+    description: The name of the dataset. This should be the url version e.g. my-dataset
+  file:
     type: string
-    description: S3 secret access key
+    description: The name of the specific file within the dataset. e.g. example.csv
 """
