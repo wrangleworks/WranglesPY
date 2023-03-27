1766beb5248b8b7afaa6579d9dc8027ebe0b9dfd
Chris Ince <chris@wrangleworks.com>
2023-03-27 15:06:39 -0500

    Add run ckan.upload and ckan.download

--- tests/connectors/test_ckan.py
+++ tests/connectors/test_ckan.py
@@ -109,3 +109,170 @@ def test_missing_file():
         raise wrangles.recipe.run(recipe)
     
     assert info.typename == 'ValueError' and info.value.args[0][:14] == 'File not found'
+
+
+def test_upload_file():
+    """
+    Test run action to upload a file
+    """
+    random_string = ''.join(random.choices(string.ascii_lowercase, k=8))
+
+    wrangles.recipe.run(
+        """
+          run:
+            on_success:
+              - ckan.upload:
+                  host: https://catalog.wrangle.works
+                  dataset: test
+                  file: tests/temp/test.csv
+                  api_key: ${CKAN_API_KEY}
+                  output_file: test.csv
+
+          read:
+            - test:
+                rows: 5
+                values:
+                  header: ${RANDOM_STRING}
+            
+          write:
+            - file:
+               name: tests/temp/test.csv
+        """,
+        variables = {
+          'RANDOM_STRING': random_string
+        }
+    )
+
+    df = wrangles.recipe.run(
+        """
+          read:
+            - ckan:
+                host: https://catalog.wrangle.works
+                dataset: test
+                file: test.csv
+                api_key: ${CKAN_API_KEY}
+        """
+    )
+
+    assert df['header'][0] == random_string
+
+def test_upload_files():
+    """
+    Test run action to upload a list of file
+    """
+    random_strings = [
+        ''.join(random.choices(string.ascii_lowercase, k=8)),
+        ''.join(random.choices(string.ascii_lowercase, k=8))
+    ]
+
+    wrangles.recipe.run(
+        """
+        run:
+          on_success:
+            - ckan.upload:
+                host: https://catalog.wrangle.works
+                dataset: test
+                file:
+                  - tests/temp/test0.csv
+                  - tests/temp/test1.csv
+                api_key: ${CKAN_API_KEY}
+                output_file:
+                  - test0.csv
+                  - test1.csv
+
+        read:
+          - test:
+              rows: 5
+              values:
+                header0: ${RANDOM_STRING_0}
+                header1: ${RANDOM_STRING_1}
+            
+        write:
+          - file:
+              name: tests/temp/test0.csv
+              columns:
+                - header0
+          - file:
+              name: tests/temp/test1.csv
+              columns:
+                - header1
+        """,
+        variables = {
+            'RANDOM_STRING_0': random_strings[0],
+            'RANDOM_STRING_1': random_strings[1],
+        }
+    )
+
+    df = wrangles.recipe.run(
+        """
+          read:
+            - concatenate:
+                sources:
+                  - ckan:
+                      host: https://catalog.wrangle.works
+                      dataset: test
+                      file: test0.csv
+                      api_key: ${CKAN_API_KEY}
+                  - ckan:
+                      host: https://catalog.wrangle.works
+                      dataset: test
+                      file: test1.csv
+                      api_key: ${CKAN_API_KEY}
+        """
+    )
+
+    assert df['header0'][0] == random_strings[0] and df['header1'][0] == random_strings[1]
+
+def test_download_file():
+    """
+    Test run action to download a file
+    """
+    df = wrangles.recipe.run(
+        """
+          run:
+            on_start:
+              - ckan.download:
+                  host: https://catalog.wrangle.works
+                  dataset: test
+                  file: test.csv
+                  api_key: ${CKAN_API_KEY}
+                  output_file: tests/temp/test.csv
+
+          read:
+            - file:
+                name: tests/temp/test.csv
+        """
+    )
+
+    assert len(df) > 0
+
+def test_download_files():
+    """
+    Test run action to download multiple files
+    """
+    df = wrangles.recipe.run(
+        """
+          run:
+            on_start:
+              - ckan.download:
+                  host: https://catalog.wrangle.works
+                  dataset: test
+                  file:
+                    - test.csv
+                    - test1.csv
+                  api_key: ${CKAN_API_KEY}
+                  output_file:
+                    - tests/temp/test.csv
+                    - tests/temp/test1.csv
+
+          read:
+            - union:
+               sources:
+                - file:
+                    name: tests/temp/test.csv
+                - file:
+                    name: tests/temp/test1.csv
+        """
+    )
+
+    assert len(df) > 0
--- wrangles/connectors/ckan.py
+++ wrangles/connectors/ckan.py
@@ -1,5 +1,6 @@
 import logging as _logging
 from io import BytesIO as _BytesIO
+from typing import Union as _Union
 import pandas as _pd
 import requests as _requests
 from . import file as _file
@@ -31,7 +32,7 @@ def read(host: str, dataset: str, file: str, api_key: str = None, **kwargs) -> _
     """
     Read data from CKAN
     
-    :param host: The host name of the CKAN site. e.g. data.example.com
+    :param host: The host name of the CKAN site. e.g. https://data.example.com
     :param dataset: The name of the dataset. This should be the url version e.g. my-dataset
     :param file: The name of the specific file within the dataset.
     :param api_key: API Key for the CKAN site.
@@ -61,7 +62,7 @@ required:
 properties:
   host:
     type: string
-    description: The host name of the CKAN site. e.g. data.example.com
+    description: The host name of the CKAN site. e.g. https://data.example.com
   dataset:
     type: string
     description: The name of the dataset. This should be the url version e.g. my-dataset
@@ -73,12 +74,12 @@ properties:
     description: API Key for the CKAN site.
 """
 
-def write(df: _pd.DataFrame, host: str, dataset: str, file: str, api_key: str = None, **kwargs):
+def write(df: _pd.DataFrame, host: str, dataset: str, file: str, api_key: str, **kwargs):
     """
     Write a file to a dataset in CKAN
     
     :param df: Dataframe to be exported
-    :param host: The host name of the CKAN site. e.g. data.example.com
+    :param host: The host name of the CKAN site. e.g. https://data.example.com
     :param dataset: The name of the dataset. This should be the url version e.g. my-dataset
     :param file: The name of the specific file within the dataset. e.g. example.csv
     :param api_key: API Key for the CKAN site.
@@ -125,7 +126,7 @@ required:
 properties:
   host:
     type: string
-    description: The host name of the CKAN site. e.g. data.example.com
+    description: The host name of the CKAN site. e.g. https://data.example.com
   api_key:
     type: string
     description: API Key for the CKAN site.
@@ -136,3 +137,166 @@ properties:
     type: string
     description: The name of the specific file within the dataset. e.g. example.csv
 """
+
+class download:
+    """
+    Download files from CKAN to the local file system
+    """
+    _schema = {
+        "run": """
+          type: object
+          description: Download data from CKAN and save to the local file system
+          required:
+            - host
+            - dataset
+            - file
+          properties:
+            host:
+              type: string
+              description: The host name of the CKAN site. e.g. https://data.example.com
+            dataset:
+              type: string
+              description: The name of the dataset. This should be the url version e.g. my-dataset
+            file:
+              type:
+                - string
+                - array
+              description: A name or list of files within the dataset. e.g. example.csv
+            api_key:
+              type: string
+              description: API Key for the CKAN site.
+            output_file:
+              type:
+                - string
+                - array
+              description: |
+                A name or list of names that the output will be saved as.
+                If omitted, defaults to the same as the file.
+          """
+    }
+
+    def run(
+        host: str,
+        dataset: str,
+        file: _Union[str, list],
+        api_key: str = None,
+        output_file: _Union[str, list] = None
+    ) -> None:
+        """
+        Download data from CKAN and save to the local file system
+        
+        :param host: The host name of the CKAN site. e.g. https://data.example.com
+        :param dataset: The name of the dataset. This should be the url version e.g. my-dataset
+        :param file: The name of the specific file within the dataset.
+        :param api_key: API Key for the CKAN site.
+        :param output_file: A name or list of names that the output will be saved as. If omitted, defaults to the same as the file.
+        """
+        _logging.info(f": Downloading data from CKAN :: {host} / {dataset} / {file}")
+        
+        if not output_file: output_file = file
+
+        if not isinstance(file, list): file = [file]
+        if not isinstance(output_file, list): output_file = [output_file]
+        
+        packages = _get_packages_in_dataset(host, dataset, api_key)
+        
+        for fname, output_fname in zip(file, output_file):
+            if fname not in packages.keys():
+                raise ValueError(f'File {fname} not found in dataset')
+            
+            # Download the requested data
+            response = _requests.get(packages[fname]["url"], headers={'Authorization': api_key})
+
+            with open(output_fname, "wb") as f:
+                f.write(_BytesIO(response.content).getbuffer())
+
+
+class upload:
+    """
+    Upload a file or list of files to a CKAN dataset
+    """
+    _schema = {
+        "run": """
+            type: object
+            description: Upload a file or list of files to a CKAN dataset
+            required:
+              - host
+              - api_key
+              - dataset
+              - file
+            properties:
+              host:
+                type: string
+                description: The host name of the CKAN site. e.g. https://data.example.com
+              api_key:
+                type: string
+                description: API Key for the CKAN site.
+              dataset:
+                type: string
+                description: The name of the dataset. This should be the url version e.g. my-dataset
+              file:
+                type:
+                  - string
+                  - array
+                description: The name of the specific file within the dataset. e.g. example.csv
+              output_file:
+                type:
+                  - string
+                  - array
+                description: |
+                  A name or list of names that the files will be saved as.
+                  If omitted, defaults to the original filename.
+        """
+    }
+
+    def run(
+        host: str,
+        dataset: str,
+        api_key: str,
+        file: _Union[str, list],
+        output_file: _Union[str, list] = None
+    ) -> None:
+        """
+        Upload a file or list of files to a CKAN dataset
+        
+        :param host: The host name of the CKAN site. e.g. https://data.example.com
+        :param dataset: The name of the dataset. This should be the url version e.g. my-dataset
+        :param api_key: API Key for the CKAN site.
+        :param file: A name or list of names of files to be uploaded. e.g. example.csv
+        :param output_file: A name or list of names that the files will be saved as. If omitted, defaults to the original filename.
+        """
+        _logging.info(f": Uploading data to CKAN :: {host} / {dataset} / {file}")
+
+        packages = _get_packages_in_dataset(host, dataset, api_key)
+
+        if not output_file: output_file = file
+
+        if not isinstance(file, list): file = [file]
+        if not isinstance(output_file, list): output_file = [output_file]
+
+        for fname, output_fname in zip(file, output_file):
+            with open(fname, "rb") as f:
+                memory_file = _BytesIO(f.read())
+
+            if output_fname in packages.keys():
+                response = _requests.post(
+                    url = f"{host}/api/action/resource_update",
+                    data = {"id": packages[output_fname]["id"]},
+                    headers = {"Authorization": api_key},
+                    files = {'upload': (output_fname, memory_file.getvalue())}
+                )
+            else:
+                response = _requests.post(
+                    url = f"{host}/api/action/resource_create",
+                    data={
+                        "package_id": dataset,
+                        "name": output_fname
+                    },
+                    headers={"Authorization": api_key},
+                    files={'upload': (output_fname, memory_file.getvalue())}
+                )
+
+            if response.status_code == 403:
+                raise RuntimeError("Access Denied to the CKAN Server. Check your API_KEY.")
+            elif response.status_code != 200:
+                raise RuntimeError(f"Upload to CKAN Failed - {fname}")
