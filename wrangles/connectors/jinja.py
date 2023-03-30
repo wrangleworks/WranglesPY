c8cefddefea6a39cb7fdbffe0d5dfa0935dd28eb
Chris Ince <chris@wrangleworks.com>
2023-03-30 12:06:02 -0500

    Rename context_dictionary to context

--- wrangles/connectors/jinja.py
+++ wrangles/connectors/jinja.py
@@ -11,11 +11,11 @@ from jinja2 import (
 _schema = {}
 
 
-def run(template: dict, context_dictionary: dict, output_file: str):
+def run(template: dict, context: dict, output_file: str):
     """
     Create a jinja template
     :param template: A generic template used to generate a more specific template to be used
-    :param context_dictionary: A dictionary used to define the output template
+    :param context: A dictionary used to define the output template
     :param output_file: File name/path for the file to be output
     """
     if 'file' in template:
@@ -33,7 +33,7 @@ def run(template: dict, context_dictionary: dict, output_file: str):
     
     # writing the file makes it easy to see what the Description template does
     with open (output_file, 'w') as f:
-        f.write(template.render(context_dictionary))
+        f.write(template.render(context))
 
 _schema['run'] = """
 type: object
@@ -41,7 +41,7 @@ description: Create a jinja template
 additionalProperties: false
 required:
   - template
-  - context_dictionary
+  - context
   - output_file
 properties:
   template:
@@ -50,7 +50,7 @@ properties:
     properties:
       file: A .jinja file containing the template
       string: A string which is used as the jinja template
-  context_dictionary:
+  context:
     type: object
     description: A dictionary used to define the output template
   output_file:
