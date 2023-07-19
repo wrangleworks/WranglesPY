3f216125e949d86eca94b9067abbf81c6025cf52
thomasstvr <65630492+thomasstvr@users.noreply.github.com>
2023-07-19 14:18:27 -0500

    Added meaningful error message to data.py

--- wrangles/data.py
+++ wrangles/data.py
@@ -47,5 +47,9 @@ def model_data(id: str, purpose: str) -> list:
     """
     params = {'model_id': id, 'type': purpose}
     response = _requests.get(f'{_config.api_host}/model/content', params=params, headers={'Authorization': f'Bearer {_auth.get_access_token()}'})
-    results = response.json()
+    if response.status_code == 200:
+        results = response.json()
+    elif response.status_code == 400:
+        raise RuntimeError('Not able to validate access, check wrangle to ensure accessibility')
+
     return results['Data']
