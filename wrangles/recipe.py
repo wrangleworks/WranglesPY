dcf1feefb11e7906a5f5e4f8fc24bdc3448c6d5c
thomasstvr <65630492+thomasstvr@users.noreply.github.com>
2023-08-29 12:37:49 -0500

    Combined functions into _read_recipe

--- wrangles/recipe.py
+++ wrangles/recipe.py
@@ -136,7 +136,7 @@ def _replace_templated_values(
     return new_recipe_object
 
 
-def _read_recipe(recipe: str):
+def _read_recipe(recipe: str, functions, variables: dict = {}) -> dict:
     """
     Read recipe from various methods (website or gist, file path, model id or string)
 
@@ -172,7 +172,31 @@ def _read_recipe(recipe: str):
         with open(recipe, "r", encoding='utf-8') as f:
             recipe_string = f.read()
 
-    return recipe_string
+        # Also add environment variables to list of placeholder variables
+    # Q: Should we exclude some?
+    for env_key, env_val in _os.environ.items():
+        if env_key not in variables.keys():
+            variables[env_key] = env_val
+
+    recipe_object = _yaml.safe_load(recipe_string)
+    
+    # Check if there are any templated valued to update
+    recipe_object = _replace_templated_values(recipe_object=recipe_object, variables=variables)
+
+        # Check that functions is an empty list and recipe is being passed as a model_id
+    if functions == [] and _re.search(r"[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}", recipe):
+        # Read functions from model_id
+        functions = _data.model_content(recipe)['functions']
+        if functions != '':
+            custom_module = _ModuleType('custom_module')
+            exec(functions, custom_module.__dict__)
+            functions = [getattr(custom_module, method) for method in dir(custom_module) if not method.startswith('_')]
+            # getting only the functions
+            functions = [x for x in functions if _isfunction(x)]
+        else:
+            functions = []
+
+    return recipe_object, functions
 
 def _load_functions(recipe: str, functions):
     """
@@ -646,9 +670,7 @@ def run(recipe: str, variables: dict = {}, dataframe: _pandas.DataFrame = None, 
     :return: The result dataframe. The dataframe can be defined using write: - dataframe in the recipe.
     """
     # Load recipe and functions
-    recipe_string = _read_recipe(recipe)
-    functions = _load_functions(recipe, functions)
-    recipe = _interpret_recipe(recipe_string, variables)
+    recipe, functions = _read_recipe(recipe, functions, variables)
 
     try:
         # Format custom functions
