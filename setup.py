8bed582d184a5379446f1c11c0aa79b82cc55f2d
Chris Ince <chris@wrangleworks.com>
2023-09-27 10:38:23 -0500

    Bump to v1.6.1

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent'
     ],
-    version = '1.6.0',
+    version = '1.6.1',
     url = 'https://github.com/wrangleworks/WranglesPy',
     author = 'WrangleWorks',
     author_email = 'chris@wrangleworks.com',
