f166dd37a5a0a474393a2aa58f08da2c8774be5c
Mario <cerdamario13@gmail.com>
2023-10-03 20:22:21 -0500

    added path capabilities to read/write

--- tests/connectors/test_google_drive.py
+++ tests/connectors/test_google_drive.py
@@ -13,53 +13,8 @@ client_id = os.getenv("GOOGLE_CLIENT_ID", "...")
 from google.oauth2 import service_account as _service_account
 from googleapiclient.discovery import build as _build
 import re as _re
-# function to delete files after they are created
-def delete_file_in_drive(file_id_or_link):
-    creds_dict = {
-        "type": "service_account",
-        "project_id": project_id,
-        "private_key_id": private_key_id,
-        "private_key": _re.sub("\\\\n", "\\n", private_key), # Remove extra back slashed from private key
-        "client_email": client_email,
-        "client_id": client_id,
-        "auth_uri": "https://accounts.google.com/o/oauth2/auth",
-        "token_uri": "https://oauth2.googleapis.com/token",
-    }
-    # Create the appropriate Google Service
-    scope = ['https://www.googleapis.com/auth/drive']
-    creds = _service_account.Credentials.from_service_account_info(
-        info=creds_dict,
-        scopes=scope,
-    )
 
-    # Authenticate and construct service.
-    service = _build(
-        'drive',
-        'v3',
-        credentials=creds
-        )
 
-    # Extract the file ID from the file path or link
-    file_id = None
-
-    # If it's a link, extract the file ID from the link
-    if 'drive.google.com' in file_id_or_link:
-        parts = file_id_or_link.split('/')
-        for i, part in enumerate(parts):
-            if part == 'd':
-                file_id = parts[i + 1]
-                break
-
-    # If it's a file ID, use it directly
-    if not file_id and file_id_or_link.startswith('file/d/'):
-        file_id = file_id_or_link[len('file/d/'):]
-        
-
-    try:
-        service.files().delete(fileId=file_id).execute()
-        print(f'File deleted: {file_id}')
-    except:
-        print(f'An Error occurred: {file_id} or file is already deleted')
 
 def test_basic_read_id():
     read_file_id = "11QLHUhnrNCRJLCARqurneAceXqlWh301" # .xlsx file
@@ -120,9 +75,25 @@ def test_reading_json_file():
         client_id=client_id
     )
     assert df.iloc[106][2] == 'MEX'
+    
+def test_reading_file_path():
+    reading_path = "Google Connector Data/Python Tests Do not Delete/Do_not_delete.xlsx"
+    df = read(
+        share_link=reading_path,
+        project_id=project_id,
+        private_key_id=private_key_id,
+        private_key=private_key,
+        client_email=client_email,
+        client_id=client_id
+    )
+    assert df.iloc[106][2] == 'MEX'
 
 
+
+#
 # write to a folder
+#
+
 folder_share_link = "https://drive.google.com/drive/folders/1a6AXyn-4PAqjaVJasr9WtQC7XY5k1G3Y?usp=drive_link"
 
 def test_basic_write_to_sheets():
@@ -215,5 +186,27 @@ def test_write_to_drive_json():
     )
     
     # the function return a dict with the file if
+    assert isinstance(file_info['id'], str)
+    
+def test_writing_file_path():
+    """
+    Write data using a path
+    """
+    folder_path = "Google Connector Data/Python Tests Do not Delete/Created Files (DELETE Files)"
+    data = _pd.DataFrame({
+        'col1': [1,2,3],
+        'col2': [4,5,6]
+    })
+    file_info = write(
+        df=data,
+        share_link= folder_path,
+        file_name='df_writing_path.json',
+        project_id=project_id,
+        private_key_id=private_key_id,
+        private_key=private_key,
+        client_email=client_email,
+        client_id=client_id,
+    )
+    
     assert isinstance(file_info['id'], str)
     
--- wrangles/connectors/google_drive.py
+++ wrangles/connectors/google_drive.py
@@ -16,6 +16,62 @@ _schema = {}
 # https://developers.google.com/identity/protocols/oauth2/service-account
 # be sure to also share the folder with the service account email address as Editor
 
+def _get_id_from_path(service, path, type):
+    """
+    Get the file ID from the the path provided
+    """
+    path_list = path.split("/")
+    folder_list = path_list[:-1]
+    if type == 'read':
+        file = _re.search(r'(\w+)\.(xlsx|csv|gsheet|json)', path)[0]
+    
+    # get the root folder id from the path
+    query = f"mimeType = 'application/vnd.google-apps.folder' and name = '{folder_list[0]}' and trashed = false"
+    results = service.files().list(q=query, fields="files(id)").execute()
+    if results['files'] == []:
+        raise ValueError(f"Folder '{folder_list[0]}' does not exist in Google Drive or does not have the correct Service Account permissions.""")
+    root_id = results['files'][0]['id']
+    
+    # Build the id_stack to keep track of folder ids
+    id_stack = root_id # initialize with the root folder id
+    file_id = ""
+    
+    # iterate through the path
+    for part in path_list[1:]:
+        
+        # Check if part is the last element in the list
+        if part == path_list[-1] and type == 'read':
+            # search for a file using the id_stack
+            query = f"'{id_stack}' in parents and mimeType != 'application/vnd.google-apps.folder'"
+            results = service.files().list(q=query, fields="files(id, name)").execute()
+            
+            # check if the file exists in last folder in the path
+            if [x['name'] for x in results['files'] if x['name'] == file]:
+                file_id = [x['id'] for x in results['files'] if x['name'] == file][0]
+                return file_id
+            
+        elif part == path_list[-1] and type == 'write':
+            query = f"'{id_stack}' in parents and mimeType = 'application/vnd.google-apps.folder'"
+            results = service.files().list(q=query, fields="files(id, name)").execute()
+            
+            # check if the file exists in last folder in the path and get id
+            if [x['name'] for x in results['files'] if x['name'] == part]:
+                file_id = [x['id'] for x in results['files'] if x['name'] == part][0]
+                return file_id
+        
+        else:
+            # Keep iterating through the path/folders. using the folder id, check that the folder contains the sub folder
+            query = f"'{id_stack}' in parents and mimeType = 'application/vnd.google-apps.folder'"
+            results = service.files().list(q=query, fields="files(id, name)").execute()
+            
+            # check if the sub folder exists in the current folder
+        if [x['name'] for x in results['files'] if x['name'] == part]:
+            # update the id_stack
+            id_stack = [x['id'] for x in results['files'] if x['name'] == part][0]
+    
+    return None # if nothing is found
+
+
 def read(
         share_link: str,
         project_id: str,
@@ -37,17 +93,7 @@ def read(
     """
     # Remove extra back slashed from private key
     private_key = _re.sub("\\\\n", "\\n", private_key)
-    
-    # check if user provided sharable link or file id
-    if 'drive.google.com' in share_link or 'docs.google.com' in share_link:
-        parts = share_link.split('/')
-        for i, part in enumerate(parts):
-            if part == 'd':
-                file_id = parts[i + 1]
-                break
-    else :
-        file_id = share_link
-    
+        
     # Credentials information
     creds_dict = {
         "type": "service_account",
@@ -74,6 +120,24 @@ def read(
         'v3',
         credentials=creds
         )
+    
+    # check if user provided sharable link or file id
+    if 'drive.google.com' in share_link or 'docs.google.com' in share_link:
+        parts = share_link.split('/')
+        for i, part in enumerate(parts):
+            if part == 'd':
+                file_id = parts[i + 1]
+                break
+    
+    # check if the share link is a path
+    elif '/' in share_link and 'https://' not in share_link:
+        file_id = _get_id_from_path(service, share_link, 'read')
+        if file_id == None:
+            raise ValueError(f"Invalid path: '{share_link}'")
+        
+    # this is just the model id
+    else :
+        file_id = share_link
     
     # Determine the mimeType based on file metadata
     file_data = service.files().get(
@@ -191,19 +255,6 @@ def write(
     # Remove extra back slashed from private key
     private_key = _re.sub("\\\\n", "\\n", private_key)
     
-    # check if user provided sharable link or folder id
-    if 'drive.google.com' in share_link:
-        parts = share_link.split('/')
-        for i, part in enumerate(parts):
-            if part == 'folders':
-                folder_id = parts[i + 1]
-                # check for any additional separators
-                if '?' in folder_id:
-                    folder_id = folder_id.split('?')[0]
-                break
-    else :
-        folder_id = share_link
-    
     # Credentials information
     creds_dict = {
         "type": "service_account",
@@ -229,7 +280,26 @@ def write(
        'drive',
         'v3',
         credentials=creds
-        )
+    )
+    
+    # check if user provided sharable link or folder id
+    if 'drive.google.com' in share_link:
+        parts = share_link.split('/')
+        for i, part in enumerate(parts):
+            if part == 'folders':
+                folder_id = parts[i + 1]
+                # check for any additional separators
+                if '?' in folder_id:
+                    folder_id = folder_id.split('?')[0]
+                break
+    
+    elif '/' in share_link and 'https://' not in share_link:
+        folder_id = _get_id_from_path(service, share_link, 'write')
+        if folder_id == None:
+            raise ValueError(f"Invalid path: '{share_link}'") 
+            
+    else :
+        folder_id = share_link
         
     # write file in memory
     memory_file = _io.BytesIO()
@@ -266,17 +336,17 @@ def write(
     # CSV file
     elif file_type in ['csv']:
         
-        mime_type = 'application/vnd.google-apps.file'
+        mime_type = 'text/csv'
         file_metadata = {
             'name': file_name,
             'parents': [folder_id],
-            'mimeType': "application/csv",
+            'mimeType': 'text/csv',
         }
         
     # JSON file
     elif file_type in ['json']:
         
-        mime_type = 'application/vnd.google-apps.file'
+        mime_type = 'application/json'
         file_metadata = {
             'name': file_name,
             'parents': [folder_id],
@@ -290,14 +360,28 @@ def write(
     media = _MediaIoBaseUpload(
         memory_file,
         mimetype=mime_type,
-        resumable=True,
+        # resumable=True,
     )
     
-    file = service.files().create(
-        body=file_metadata,
-        media_body=media,
-        fields='id',
+    # Check if the file already exists in folder
+    results = service.files().list(
+        q=f"'{folder_id}' in parents and name='{file_name}'",
+        fields="files(id)",
     ).execute()
+    
+    # if file exists, then replace it
+    if results['files'] != []:
+        file = service.files().update(
+            fileId=results['files'][0]['id'],
+            media_body=media,
+        ).execute()
+    else:
+        # upload a new file
+        file = service.files().create(
+            body=file_metadata,
+            media_body=media,
+            fields='id',
+        ).execute()
     
     return file
 
