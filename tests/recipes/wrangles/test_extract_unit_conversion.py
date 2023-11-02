50380d4b759e73ba788f5f2d3685257b902f37c9
cerdamario13 <cerdamario13@gmail.com>
2023-11-02 09:59:20 -0500

    finished object tests

--- tests/recipes/wrangles/test_extract_unit_conversion.py
+++ tests/recipes/wrangles/test_extract_unit_conversion.py
@@ -481,6 +481,29 @@ def test_volumetric_flow_rate_conversion_1():
     )
     assert df['out'].tolist() == [['2.64 gpm'], ['10.0 gpm'], ['74.8 gpm'], ['2.64 gpm']]
     
+def test_volumetric_flow_rate_conversion_2_obj():
+    """
+    Test multiple units of flow rate object
+    """
+    data = pd.DataFrame(
+        {'col': ['10 liter per minute', '10 gallon per minute', '10 cubic feet per minute', '10 liter per minute']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out
+              attribute_type: volumetric flow
+              desired_unit: gallon per minute
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][1] == [{'symbol': 'gpm', 'unit': 'gallon per minute', 'value': 10.0}]
+    
+    
+    
 #
 # Length
 #
@@ -504,6 +527,27 @@ def test_length_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['0.621 mi'], ['0.621 mi'], ['0.0621 mi'], ['0.0158 mi'], ['0.568 mi'], ['1.0 mi'], ['0.932 mi'], ['0.189 mi']]
+    
+def test_length_conversion_2_obj():
+    """
+    Test multiple units of length
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 meters', '1000 m', '10000 cm', '1000 inches', '1000 yards', '1 mile', '1.5 km', '1000 ft']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out
+              attribute_type: length
+              desired_unit: miles
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'mi', 'unit': 'mile', 'value': 0.621}]
     
     
 #
@@ -530,6 +574,27 @@ def test_weight_conversion_1():
     )
     assert df['out'].tolist() == [['220 lb'], ['100 lb'], ['100 lb'], ['100 lb'], ['2.2 lb'], ['22.0 lb'], ['220 lb'], ['0.22 lb']]
     
+def test_weight_conversion_2_obj():
+    """
+    Test multiple units of weight
+    """
+    data = pd.DataFrame(
+        {'col': ['100 kg', '100 lbs', '100 lb', '100 pounds', '1000 g', '10000 grams', '100 kilograms', '100000 mg']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: weight
+              desired_unit: pounds
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][4] == [{'symbol': 'lb', 'unit': 'pound-mass', 'value': 2.2}]
+    
     
 def test_voltage_conversion_1():
     """
@@ -550,6 +615,27 @@ def test_voltage_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['1.0 V'], ['1.0 V'], ['1.0 V'], ['13.0 V']]
+    
+def test_voltage_conversion_2_obj():
+    """
+    Test multiple units of voltage
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 millivolts', '1000 mV', '1000 mV DC', '13 V AC']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: voltage
+              desired_unit: volts
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][2] == [{'symbol': 'V', 'unit': 'volt', 'value': 1.0}]
     
 #
 # degree
@@ -622,6 +708,27 @@ def test_capacitance_conversion_1():
     )
     assert df['out'].tolist() == [['0.1 F'], ['0.1 F'], ['10.0 F'], ['10.0 F']]
     
+def test_capacitance_conversion_2_obj():
+    """
+    Test multiple units of capacitance
+    """
+    data = pd.DataFrame(
+        {'col': ['100000 microFarad', '100000 µF', '10 Farad', '10 farad']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: capacitance
+              desired_unit: farads
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'F', 'unit': 'farad', 'value': 0.1}]
+    
     
 #
 # Frequency
@@ -647,6 +754,27 @@ def test_frequency_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['1.0 kHz'], ['1.0 kHz'], ['100 kHz'], ['100 kHz']]
+    
+def test_frequency_conversion_2_obj():
+    """
+    Test multiple units of frequency
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 hertz', '1000 Hz', '100 kHz', '0.1 MHz']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: frequency
+              desired_unit: kilohertz
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][3] == [{'symbol': 'kHz', 'unit': 'kilohertz', 'value': 100.0}]
 
 
 #
@@ -672,6 +800,27 @@ def test_speed_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['44.7 mps'], ['27.8 mps'], ['100 mps'], ['100 mps'], ['30.5 mps'], ['30.5 mps']]
+    
+def test_speed_conversion_2_obj():
+    """
+    Test multiple units of speed
+    """
+    data = pd.DataFrame(
+        {'col': ['100 mph', '100 kph', '100 meters per second', '100 m/s', '100 feet per second', '100 ft/s']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: speed
+              desired_unit: meters per second
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][2] == [{'symbol': 'mps', 'unit': 'metre per second', 'value': 100.0}]
 
 
 #
@@ -697,6 +846,27 @@ def test_charge_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['10.0 C'], ['10.0 C']]
+    
+def test_charge_conversion_2_obj():
+    """
+    Test multiple units of charge
+    """
+    data = pd.DataFrame(
+        {'col': ['10 coulomb', '10000 mC']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: charge
+              desired_unit: coulomb
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'C', 'unit': 'coulomb', 'value': 10.0}]
 
 
 #
@@ -724,6 +894,27 @@ def test_data_transfer_rate_conversion_1():
     )
     assert df['out'].tolist() == [['0.01 Gbps'], ['0.001 Gbps'], ['1.0 Gbps']]
     
+def test_data_transfer_rate_conversion_2_obj():
+    """
+    Test multiple units of data transfer rate
+    """
+    data = pd.DataFrame(
+        {'col': ['10000000 bps', '1000 kbps', '1 gbps']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: data transfer rate
+              desired_unit: gigabit per second
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'Gbps', 'unit': 'gigabit per second', 'value': 0.01}]
+    
 
 #
 # Electrical conductance
@@ -750,6 +941,27 @@ def test_electrical_conductance_conversion_1():
     )
     assert df['out'].tolist() == [['100 S']]
     
+def test_electrical_conductance_conversion_2_obj():
+    """
+    Test multiple units of electrical conductance. Be sure to update to have kilo and milli
+    """
+    data = pd.DataFrame(
+        {'col': ['100 siemens']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: electrical conductance
+              desired_unit: siemens
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'S', 'unit': 'siemens', 'value': 100.0}]
+    
     
 #
 # Inductance
@@ -775,6 +987,27 @@ def test_inductance_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['1000 H'], ['1000 H']]
+    
+def test_inductance_conversion_2_obj():
+    """
+    Test multiple units of inductance. Be sure to update to have kilo and milli
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 henry', '1000 H']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: inductance
+              desired_unit: henry
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'H', 'unit': 'henry', 'value': 1000.0}]
 
 
 #
@@ -801,6 +1034,27 @@ def test_frequency_conversion_2():
         dataframe=data
     )
     assert df['out'].tolist() == [['1000 rpm'], ['60000 rpm']]
+    
+def test_frequency_conversion_2_obj():
+    """
+    Test multiple units of frequency. Be sure to update to have kilo and milli
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 rpm', '1000 cps']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: instance frequency
+              desired_unit: revolutions per minute
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'rpm', 'unit': 'revolutions per minute', 'value': 1000.0}]
     
     
     
@@ -829,6 +1083,27 @@ def test_lumens_conversion_1():
     )
     assert df['out'].tolist() == [['1000 lm'], ['1000 lm']]
     
+def test_lumens_conversion_2_obj():
+    """
+    Test multiple units of Lumens. Be sure to update to have kilo and milli
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 lumens', '1000 lm']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out 
+              attribute_type: luminous flux
+              desired_unit: lumens
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][0] == [{'symbol': 'lm', 'unit': 'lumen', 'value': 1000.0}]
+    
     
 #
 # electrical resistance
@@ -854,6 +1129,27 @@ def test_ohms_conversion_1():
     )
     assert df['out'].tolist() == [['10.0 Ω'], ['10.0 Ω'], ['10.0 Ω'], ['10.0 Ω'], ['1000 Ω']]
     
+def test_ohms_conversion_2_obj():
+    """
+    Test multiple units of Lumens
+    """
+    data = pd.DataFrame(
+        {'col': ['10 ohms', '10 Ω', '10000 milliohms', '10000 mΩ', '1 kΩ']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col 
+              output: out
+              attribute_type: electrical resistance
+              desired_unit: ohms
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][3] == [{'symbol': 'Ω', 'unit': 'ohm', 'value': 10.0}]
+    
 
 #
 # Energy
@@ -878,3 +1174,26 @@ def test_energy_conversion_1():
         dataframe=data
     )
     assert df['out'].tolist() == [['1000 J'], ['1000 J'], ['10000 J'], ['3600000 J']]
+    
+def test_energy_conversion_2_obj():
+    """
+    Test multiple units of Lumens, be sure to add BTU and Calories are all kilocalories
+    """
+    data = pd.DataFrame(
+        {'col': ['1000 joules', '1000 J', '10 kilojoules', '1 kwh']}
+    )
+    df = wrangles.recipe.run(
+        recipe="""
+        wrangles:
+          - extract.attributes:
+              input: col
+              output: out
+              attribute_type: energy
+              desired_unit: joules
+              responseContent: object
+        """,
+        dataframe=data
+    )
+    assert df['out'][3] == [{'symbol': 'J', 'unit': 'joule', 'value': 3600000}]
+    
+    
