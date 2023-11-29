d81a6eae5c08ab62bd48ee419d951e39efc15d76
Chris Ince <chris@ince.live>
2023-11-29 15:34:05 -0600

    Add excel.sheet connector

---
+++ tests/connectors/test_excel.py
@@ -0,0 +1,32 @@
+import wrangles
+from wrangles.connectors import memory
+
+
+def test_default_write():
+    """
+    Test memory connector
+    without setting an ID
+    """
+    memory.dataframes = {}
+    wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                header1: value1
+                header2: value2
+        
+        write:
+          - excel.sheet: {}
+        """
+    )
+    data = [
+        v
+        for _, v in memory.dataframes.items()
+        if v.get("wranglesxl_connector") == "excel.sheet.write"
+    ][0]
+    assert (
+        data["columns"] == ["header1", "header2"] and
+        len(data["data"]) == 5
+    )
--- wrangles/connectors/__init__.py
+++ wrangles/connectors/__init__.py
@@ -4,6 +4,7 @@ Connectors to read/write from external systems
 
 from . import akeneo
 from . import ckan
+from . import excel
 from . import file
 from . import http
 from . import memory
