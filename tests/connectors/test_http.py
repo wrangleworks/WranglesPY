216c76213bf710edeb35298ce8e2a6a9c39f14c1
Chris Ince <chris@wrangleworks.com>
2024-04-23 14:46:24 -0500

    Add write, oauth support, pass through kwargs to requests, orient option for JSON structure

--- tests/connectors/test_http.py
+++ tests/connectors/test_http.py
@@ -25,3 +25,48 @@ def test_run():
     from wrangles.connectors import http
     test = http.run('https://pokeapi.co/api/v2/pokemon/charizard')
     assert test == None
+
+def test_read_oauth():
+    """
+    Test reading data using OAuth
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - http:
+              url: https://api.wrangle.works/model/content
+              params:
+                model_id: fc7d46e3-057f-47bd
+              method: GET
+              oauth:
+                url: https://sso.wrangle.works/auth/realms/wrwx/protocol/openid-connect/token
+                method: POST
+                data: grant_type=password&username=${WRANGLES_USER}&password=${WRANGLES_PASSWORD}&client_id=services
+                headers:
+                  Content-Type: application/x-www-form-urlencoded
+        """
+    )
+    assert df.columns.tolist() == ["Columns", "Data", "Settings"]
+
+def test_read_orient_tight():
+    """
+    Test reading data using OAuth
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - http:
+              url: https://api.wrangle.works/model/content
+              params:
+                model_id: fc7d46e3-057f-47bd
+              method: GET
+              orient: tight
+              oauth:
+                url: https://sso.wrangle.works/auth/realms/wrwx/protocol/openid-connect/token
+                method: POST
+                data: grant_type=password&username=${WRANGLES_USER}&password=${WRANGLES_PASSWORD}&client_id=services
+                headers:
+                  Content-Type: application/x-www-form-urlencoded
+        """
+    )
+    assert df.columns.tolist() == ["Find", "Replace", "Notes"]
