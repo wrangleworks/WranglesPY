255989362ae4126eebf9c5277933a6dc82935c28
Mario Cerda <43070036+cerdamario13@users.noreply.github.com>
2024-07-15 16:05:23 +0000

    merge tests

--- tests/recipes/wrangles/test_merge.py
+++ tests/recipes/wrangles/test_merge.py
@@ -46,6 +46,54 @@ def test_coalesce_where():
     df = wrangles.recipe.run(recipe, dataframe=data)
     assert df.iloc[1]['Output Col'] == 'B' and df.iloc[0]['Output Col'] ==''
 
+def test_coalesce_empty_dataframe():
+    """
+    Test coalesce using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.coalesce:
+              input: 
+                - Col1
+                - Col2
+                - Col3
+              output: Output Col
+        """,
+        dataframe=pd.DataFrame({
+            'Col1': [],
+            'Col2': [],
+            'Col3': []
+        })
+    )
+    assert len(df) == 0 and "Output Col" in df.columns
+
+def test_coalesce_where_empty():
+    """
+    Test coalesce using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                Col1: ['a', 'b', 'c', 'd', 'e']
+                Col2: ['f', 'g', 'h', 'i', 'j']
+                Col3: ['k', 'l', 'm', 'n', 'o']
+        wrangles:
+          - merge.coalesce:
+              input: 
+                - Col1
+                - Col2
+                - Col3
+              output: Output Col
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['Output Col'].values.tolist())
+
 #
 # Concatenate
 #
@@ -206,6 +254,56 @@ def test_concatenate_skip_empty_default():
     )
     assert df["result"][0] == 'a--b'
 
+def test_concatenate_empty_dataframe():
+    """
+    Test concatenate using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.concatenate:
+              input: 
+                - Col1
+                - Col2
+                - Col3
+              output: Join Col
+              char: ', '
+        """,
+        dataframe=pd.DataFrame({
+            'Col1': [],
+            'Col2': [],
+            'Col3': []
+        })
+    )
+    assert len(df) == 0 and "Join Col" in df.columns
+
+def test_concatenate_where_empty():
+    """
+    Test concatenate using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                Col1: ['a', 'b', 'c', 'd', 'e']
+                Col2: ['f', 'g', 'h', 'i', 'j']
+                Col3: ['k', 'l', 'm', 'n', 'o']
+        wrangles:
+          - merge.concatenate:
+              input: 
+                - Col1
+                - Col2
+                - Col3
+              output: Join Col
+              char: ', '
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['Join Col'].values.tolist())
+
 #
 # Lists
 #
@@ -246,6 +344,50 @@ def test_lists_where():
     """
     df = wrangles.recipe.run(recipe, dataframe=data)
     assert df.iloc[0]['Combined Col'] == '' and df.iloc[1]['Combined Col'] == ['C', 'D', 'F', 'G']
+
+def test_lists_empty_dataframe():
+    """
+    Test merge.lists using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.lists:
+              input: 
+                - Col1
+                - Col2
+              output: Combined Col
+        """,
+        dataframe=pd.DataFrame({
+            'Col1': [],
+            'Col2': []
+        })
+    )
+    assert len(df) == 0 and "Combined Col" in df.columns
+
+def test_lists_where_empty():
+    """
+    Test merge.lists using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                Col1: [['a', 'b']]
+                Col2: [['c', 'd']]
+        wrangles:
+          - merge.lists:
+              input: 
+                - Col1
+                - Col2
+              output: Combined Col
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['Combined Col'].values.tolist())
 
 #
 # to_list
@@ -291,6 +433,54 @@ def test_to_lists_where():
     """
     df = wrangles.recipe.run(recipe, dataframe=data)
     assert df.iloc[0]['Combined Col'] == '' and df.iloc[1]['Combined Col'] == ['D', 'E', 'F']
+
+def test_to_lists_empty_dataframe():
+    """
+    Test merge.to_list using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.to_list:
+              input: 
+                - Col1
+                - Col2
+                - Col3
+              output: Combined Col
+        """,
+        dataframe=pd.DataFrame({
+            'Col1': [],
+            'Col2': [],
+            'Col3': []
+        })
+    )
+    assert len(df) == 0 and "Combined Col" in df.columns
+
+def test_to_lists_where_empty():
+    """
+    Test merge.to_list using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                Col1: ['a', 'b', 'c', 'd', 'e']
+                Col2: ['f', 'g', 'h', 'i', 'j']
+                Col3: ['k', 'l', 'm', 'n', 'o']
+        wrangles:
+          - merge.to_list:
+              input: 
+                - Col1
+                - Col2
+                - Col3
+              output: Combined Col
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['Combined Col'].values.tolist())
 
 #
 # To Dict
@@ -362,6 +552,50 @@ def test_to_dict_where():
     """
     df = wrangles.recipe.run(recipe, dataframe=data)
     assert df.iloc[0]['Dict Col'] == {'Col1': 'A', 'Col2': 'B'} and df.iloc[2]['Dict Col'] == ''
+
+def test_to_dict_empty_dataframe():
+    """
+    Test merge.to_dict using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.to_dict:
+              input: 
+                - Col1
+                - Col2
+              output: Dict Col
+        """,
+        dataframe=pd.DataFrame({
+            'Col1': [],
+            'Col2': []
+        })
+    )
+    assert len(df) == 0 and "Dict Col" in df.columns
+
+def test_to_dict_where_empty():
+    """
+    Test merge.to_dict using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                Col1: ['a', 'b', 'c', 'd', 'e']
+                Col2: ['f', 'g', 'h', 'i', 'j']
+        wrangles:
+          - merge.to_dict:
+              input: 
+                - Col1
+                - Col2
+              output: Dict Col
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['Dict Col'].values.tolist())
 
 #
 # Key Value Pairs
@@ -436,6 +670,48 @@ def test_key_value_pairs_3():
     """
     df = wrangles.recipe.run(recipe, dataframe=data)
     assert df.iloc[0]['Object'] == {'A': 'a', True: False}
+
+def test_empty_dataframe():
+    """
+    Test merge.key_value_pairs using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.key_value_pairs:
+              input:
+                key1: value1
+              output: output
+        """,
+        dataframe=pd.DataFrame({
+            'key1': [],
+            'value1': []
+        })
+    )
+    assert len(df) == 0 and "output" in df.columns
+
+def test_key_value_pairs_where_empty():
+    """
+    Test merge.key_value_pairs using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                key1: ['a', 'b', 'c', 'd', 'e']
+                value1: ['f', 'g', 'h', 'i', 'j']
+        wrangles:
+          - merge.key_value_pairs:
+              input:
+                key1: value1
+              output: output
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['output'].values.tolist())
     
 #
 # Dictionaries
@@ -477,3 +753,47 @@ def test_merge_dicts_where():
     """
     df = wrangles.recipe.run(recipe, dataframe=data)
     assert df.iloc[1]['out'] == {'Hello': 'Moto', 'Hola': 'Hello'} and df.iloc[0]['out'] == ''
+
+def test_merge_dictionaries_empty_dataframe():
+    """
+    Tests merge.dictionaries using an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        wrangles:
+          - merge.dictionaries:
+              input: 
+                - d1
+                - d2
+              output: output
+        """,
+        dataframe=pd.DataFrame({
+            'd1': [],
+            'd2': []
+        })
+    )
+    assert len(df) == 0 and "output" in df.columns
+
+def test_merge_dictionaries_where_empty():
+    """
+    Tests merge.dictionaries using where and an empty dataframe
+    """
+    df = wrangles.recipe.run(
+        """
+        read:
+          - test:
+              rows: 5
+              values:
+                d1: [{'a': 1}]
+                d2: [{'a': 2}]
+        wrangles:
+          - merge.dictionaries:
+              input: 
+                - d1
+                - d2
+              output: output
+              where: 1 = 2
+        """
+    )
+    # All values should be empty
+    assert all(x=="" for x in df['output'].values.tolist())
