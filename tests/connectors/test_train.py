be3a9481d392e36082a8ecd4eb969b78f5a5ffc1
Christopher Ince <58178530+ChrisWRWX@users.noreply.github.com>
2024-09-17 16:35:53 +0000

    Remove mocked tests

--- tests/connectors/test_train.py
+++ tests/connectors/test_train.py
@@ -174,39 +174,6 @@ def test_extract_write_2():
             })
         )
 
-def test_extract_read_two_cols_wrgl(mocker):
-    """
-    Wrangle that contains only two columns
-    """
-    m1 = mocker.patch("wrangles.data.model_content")
-    m1.return_value = {
-        "Data": [['Hello', 'Wrangles'], ['Hello', 'Python']]
-    }
-    recipe = """
-    read:
-      - train.extract:
-          model_id: a62c7480-500e-480c
-    """
-    df = wrangles.recipe.run(recipe)
-    assert df.iloc[0].tolist() == ['Hello', 'Wrangles', '']
-
-def test_extract_read_four_cols_error(mocker):
-    """
-    Wrangles that does not contain 3 columns
-    """
-    m1 = mocker.patch("wrangles.data.model_content")
-    m1.return_value = {
-        "Data": [['Hello']]
-    }
-    with pytest.raises(ValueError, match="contain three columns"):
-        wrangles.recipe.run(
-            """
-            read:
-            - train.extract:
-                model_id: a62c7480-500e-480c
-            """
-        )
-
 def test_extract_error():
     """
     Not Providing model_id or name in train wrangles
@@ -288,40 +255,6 @@ def test_standardize_write_2():
         'Notes2': ['For pytests', ''],
         })
     )
-
-def test_standardize_read_two_cols_wrgl(mocker):
-    """
-    Wrangle that contains only two columns
-    """
-    m1 = mocker.patch("wrangles.data.model_content")
-    m1.return_value = {
-        "Data": [['Hello', 'Wrangles'], ['Hello', 'Python']]
-    }
-    df = wrangles.recipe.run(
-        """
-        read:
-        - train.standardize:
-            model_id: a62c7480-500e-480c
-        """
-    )
-    assert df.iloc[0].tolist() == ['Hello', 'Wrangles', '']
-
-def test_standardize_read_incorrect_cols_error(mocker):
-    """
-    wrangles that does not contain 3 columns
-    """
-    m1 = mocker.patch("wrangles.data.model_content")
-    m1.return_value = {
-        "Data": [['Hello']]
-    }
-    with pytest.raises(ValueError, match="contain three columns"):
-        wrangles.recipe.run(
-            """
-            read:
-            - train.standardize:
-                model_id: a62c7480-500e-480c
-            """
-        )
 
 def test_standardize_error():
     """
