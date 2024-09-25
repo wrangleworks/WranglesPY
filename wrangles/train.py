a7bf3aff2ab4e603a4ef785e32c553c3eb6dea16
Thomas K Stover <thomasstvr@gmail.com>
2024-09-25 18:36:37 +0000

    Updated error handling to only look for Key column when using a key based lookup

--- wrangles/train.py
+++ wrangles/train.py
@@ -100,7 +100,7 @@ class train():
                 raise ValueError(
                     "Lookup: The data must be a dictionary of the format {'Data': [[]], 'Columns': [], 'Settings': {}}"
                 )
-            if "Key" not in data["Columns"]:
+            if settings['variant'] =='key' and "Key" not in data["Columns"]:
                 raise ValueError("Lookup: Data must contain one column named Key")
             
         if name is not None and settings.get("variant", "") not in ["key", "embedding", "fuzzy", "recipe"]:
