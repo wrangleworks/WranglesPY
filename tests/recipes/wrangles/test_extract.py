accd30c5f17b328b17c85ae56638152653f055bf
Thomas K Stover <thomasstvr@gmail.com>
2024-10-03 17:46:24 +0000

    Added .upper().replace(' ', '') to ensure that test passes no matter what variation extract.ai returns

--- tests/recipes/wrangles/test_extract.py
+++ tests/recipes/wrangles/test_extract.py
@@ -2085,9 +2085,9 @@ def test_ai_messages():
     # This is temperamental, and sometimes GPT returns lowercase
     # Score as 2/3 as good enough for test to pass
     matches = sum([
-        df['length'][0] == '25MM',
-        df['length'][1] == '6M',
-        df['length'][2] == '3MM'
+        df['length'][0].upper().replace(' ', '') == '25MM',
+        df['length'][1].upper().replace(' ', '') == '6M',
+        df['length'][2].upper().replace(' ', '') == '3MM'
     ])
     assert matches >= 2
 
