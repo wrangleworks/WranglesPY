949ce81cd38583b0ca6ec73c0418b39cb54cf139
Christopher Ince <58178530+ChrisWRWX@users.noreply.github.com>
2024-10-07 22:05:25 +0000

    Additional tests

--- tests/connectors/test_concurrent.py
+++ tests/connectors/test_concurrent.py
@@ -1,16 +1,85 @@
 import wrangles
 from datetime import datetime
 import time
+import pandas as pd
 
-def test_write():
+def wait_and_read(duration):
+    time.sleep(duration)
+    return pd.DataFrame({"column": [f"value{str(duration)}"]})
+
+def test_read_multithread():
+    """
+    Test using the concurrent connector to read multithreaded
+    """    
+    start = datetime.now()
+
+    df = wrangles.recipe.run(
+        """
+        read:
+          - union:
+              sources:
+                - concurrent:
+                    read:
+                    - custom.wait_and_read:
+                        duration: 5
+                    - custom.wait_and_read:
+                        duration: 1
+                    - custom.wait_and_read:
+                        duration: 3
+        """,
+        functions=wait_and_read
+    )
+
+    end = datetime.now()
+
+    assert (
+        (end - start).seconds < 6 and
+        len(df) == 3
+    )
+
+def test_read_multiprocess():
+    """
+    Test using the concurrent connector to read using multiprocessing
+    """    
+    start = datetime.now()
+
+    df = wrangles.recipe.run(
+        """
+        read:
+          - union:
+              sources:
+                - concurrent:
+                    use_multiprocessing: true
+                    read:
+                    - custom.wait_and_read:
+                        duration: 5
+                    - custom.wait_and_read:
+                        duration: 1
+                    - custom.wait_and_read:
+                        duration: 3
+        """,
+        functions=wait_and_read
+    )
+
+    end = datetime.now()
+
+    assert (
+        (end - start).seconds < 6 and
+        len(df) == 3
+    )
+
+test_vals = {
+    "multithread": [],
+    "multiprocess": []
+}
+def wait_and_append(df, wait, test_vals_key):
+    time.sleep(wait)
+    test_vals[test_vals_key].append(wait)
+
+def test_write_multithread():
     """
     Test using the concurrent connector to write
     """
-    save_vals = []
-    def wait_and_append(df, wait):
-        time.sleep(wait)
-        save_vals.append(wait)
-    
     start = datetime.now()
 
     wrangles.recipe.run(
@@ -25,10 +94,13 @@ def test_write():
               write:
                 - custom.wait_and_append:
                      wait: 5
+                     test_vals_key: multithread
                 - custom.wait_and_append:
                      wait: 1
+                     test_vals_key: multithread
                 - custom.wait_and_append:
                      wait: 3
+                     test_vals_key: multithread
         """,
         functions=wait_and_append
     )
@@ -36,20 +108,23 @@ def test_write():
     end = datetime.now()
 
     assert (
-        (end - start).seconds < 6 and
-        save_vals == [1,3,5]
+        5 <= (end - start).seconds < 6 and
+        test_vals["multithread"] == [1,3,5]
     )
 
+def sleep(seconds):
+    """
+    As a builtin function implemented in C,
+    time.sleep() is not directly accessible to the recipe engine.
+    Wrap as a python function to make the function definition available.
+    """
+    time.sleep(seconds)
 
-def test_run():
+def test_run_multithread():
     """
     Test using the concurrent connector to run
+    using multithreading
     """
-    save_vals = []
-    def wait_and_append(wait):
-        time.sleep(wait)
-        save_vals.append(wait)
-    
     start = datetime.now()
 
     wrangles.recipe.run(
@@ -58,19 +133,44 @@ def test_run():
           on_start:
           - concurrent:
               run:
-                - custom.wait_and_append:
-                     wait: 5
-                - custom.wait_and_append:
-                     wait: 1
-                - custom.wait_and_append:
-                     wait: 3
+                - custom.sleep:
+                     seconds: 5
+                - custom.sleep:
+                     seconds: 1
+                - custom.sleep:
+                     seconds: 3
         """,
-        functions=wait_and_append
+        functions=sleep
     )
 
     end = datetime.now()
 
-    assert (
-        (end - start).seconds < 6 and
-        save_vals == [1,3,5]
+    assert 5 <= (end - start).seconds < 6
+
+def test_run_multiprocess():
+    """
+    Test using the concurrent connector to run
+    using multiprocessing
+    """
+    start = datetime.now()
+
+    wrangles.recipe.run(
+        """
+        run:
+          on_start:
+          - concurrent:
+              use_multiprocessing: true
+              run:
+                - custom.sleep:
+                     seconds: 5
+                - custom.sleep:
+                     seconds: 1
+                - custom.sleep:
+                     seconds: 3
+        """,
+        functions=sleep
     )
+
+    end = datetime.now()
+
+    assert 5 <= (end - start).seconds < 6
