1132349297c3b81880a3e96b2c757b5f924196d7
Brady Edwards <34783343+kavu16@users.noreply.github.com>
2025-02-28 17:35:01 +0000

    Read from actual sqlite db from samples and write to db in temp

--- tests/connectors/test_sqlite.py
+++ tests/connectors/test_sqlite.py
@@ -4,23 +4,20 @@ import pandas as pd
 from wrangles.connectors.sqlite import read, write
 
 def test_read_sql(mocker):
-    data = pd.DataFrame({'Col1': ['Data1, Data2']})
-    m = mocker.patch("pandas.read_sql")
-    m.return_value = data
+    data = pd.DataFrame({'Col1': ['Data1', 'Data2'], 'Col2': [1, 2]})
     config = {
-        'database': 'database.db',
+        'database': './tests/samples/test.db',
         'command': 'SELECT * from df_mock',
     }
     df = read(**config)
     assert df.equals(data)
 
 def test_write_sql(mocker):
-    m = mocker.patch("pandas.DataFrame.to_sql")
-    m.return_value = None
+    mock_df = pd.DataFrame({'Col1': ['Data1', 'Data2']})
     config = {
-        'df': pd.DataFrame({'insert': ['first', 'second']}),
-        'database': 'database.db',
-        'table': 'WrWx'
+        'df': mock_df,
+        'database': './tests/temp/temp.db',
+        'table': 'temp_mock'
     }
-    df = write(**config)
-    assert df == None
+    write(**config)
+    assert read(database='./tests/temp/temp.db', command='SELECT * from temp_mock').equals(mock_df)
