0407713e30da7a042212831f42ef215f45ccbd04
Christopher Ince <58178530+ChrisWRWX@users.noreply.github.com>
2025-03-11 19:48:22 +0000

    Misc improvements to sqlite connector

--- tests/connectors/test_sqlite.py
+++ tests/connectors/test_sqlite.py
@@ -1,23 +1,34 @@
 import pandas as pd
+from wrangles.connectors import sqlite
 
-# Testing sqlite connectors
-from wrangles.connectors.sqlite import read, write
+def test_read_sql():
+    """
+    Test basic read
+    """
+    df = sqlite.read(
+        database = './tests/samples/test.db',
+        command = 'SELECT * from df_mock'
+    )
+    assert df.equals(
+        pd.DataFrame(
+            {
+                'Col1': ['Data1', 'Data2'],
+                'Col2': [1, 2]
+            }
+        )
+    )
 
-def test_read_sql(mocker):
-    data = pd.DataFrame({'Col1': ['Data1', 'Data2'], 'Col2': [1, 2]})
-    config = {
-        'database': './tests/samples/test.db',
-        'command': 'SELECT * from df_mock',
-    }
-    df = read(**config)
-    assert df.equals(data)
-
-def test_write_sql(mocker):
-    mock_df = pd.DataFrame({'Col1': ['Data1', 'Data2']})
-    config = {
-        'df': mock_df,
-        'database': './tests/temp/temp.db',
-        'table': 'temp_mock'
-    }
-    write(**config)
-    assert read(database='./tests/temp/temp.db', command='SELECT * from temp_mock').equals(mock_df)
+def test_write_sql():
+    """
+    Test a basic read
+    """
+    df = pd.DataFrame({'Col1': ['Data1', 'Data2']})
+    sqlite.write(
+        df = df,
+        database = './tests/temp/temp.db',
+        table = 'temp_mock'
+    )
+    assert sqlite.read(
+        database='./tests/temp/temp.db',
+        command='SELECT * from temp_mock'
+    ).equals(df)
--- wrangles/connectors/sqlite.py
+++ wrangles/connectors/sqlite.py
@@ -2,37 +2,29 @@
 Connector to read/write from SQLite database
 """
 import pandas as _pd
-from typing import Union as _Union
 import logging as _logging
 import sqlite3 as _sqlite3
 
 _schema = {}
 
-def read(database: str, command: str, columns: _Union[str, list] = None, params: _Union[list, dict] = None) -> _pd.DataFrame:
+def read(database: str, command: str, **kwargs) -> _pd.DataFrame:
     """
     Import data from a SQLite database.
 
     >>> from wrangles.connectors import sqlite
     >>> df = sqlite.read(database = 'database.db', command='SELECT * FROM table')
 
-
     :param database: Database to be queried
     :param command: SQL command or table name
-    :param columns: (Optional) Subset of columns to be returned. This is less efficient than specifying in the SQL command.
-    :param params: (Optional) List of parameters to pass to execute method. The syntax used to pass parameters is database driver dependent.
-    :return: Pandas Dataframe of the imported data
     """
-
     _logging.info(f": Reading data from SQLite :: {database}")
     
-    conn = _sqlite3.connect(database)
-    df = _pd.read_sql(command, conn, params)
-
-    if columns is not None: df = df[columns]
+    with _sqlite3.connect(database) as conn:
+        df = _pd.read_sql(command, conn, **kwargs)
 
     return df
 
-_schema['read'] = """
+_schema['read'] = r"""
 type: object
 description: Import data from a SQLite Database
 required:
@@ -49,58 +41,43 @@ properties:
       Note - using variables here can make your recipe vulnerable
       to sql injection. Use params if using variables from
       untrusted sources.
-  columns:
-    type: array
-    description: A list with a subset of the columns to import. This is less efficient than specifying in the command.
-  params:
-    type: 
-      - array
-      - object
-    description: |-
-      List of parameters to pass to execute method.
-      This may use %s or %(name)s syntax
 """
 
-def write(df: _pd.DataFrame, database: str, table: str, action = 'INSERT', columns: _Union[str, list] = None) -> None:
+def write(df: _pd.DataFrame, database: str, table: str, **kwargs) -> None:
     """
     Write data to a SQLite database.
 
     >>> from wrangles.connectors import sqlite
-    >>> sqlite.write(df, database = 'database.db')
+    >>> sqlite.write(df, database = 'database.db', table = 'table')
 
     :param df: Pandas Dataframe to be written
-    :param database: Database to be written to
+    :param database: The database to connect to including the file path. e.g. directory/database.db
     :param table: Table to be exported to
-    :param action: Action to be taken on the database, defaults to 'INSERT'. Options are 'INSERT', 'UPDATE', 'DELETE', etc
-    :param columns: (Optional) Subset of columns to be written. If not provided, all columns will be output
     """
+    _logging.info(f": Writing data to SQLite :: {database} / {table}")
 
-    _logging.info(f": Writing data to SQLite :: {database}")
-
-    conn = _sqlite3.connect(database)
-    df.to_sql(table, conn, if_exists = 'append', index = False, method='multi', chunksize=1000)
+    with _sqlite3.connect(database) as conn:
+        df.to_sql(
+            table,
+            con = conn,
+            if_exists = 'append',
+            index = False,
+            method='multi',
+            chunksize=1000,
+            **kwargs
+        )
 
 _schema['write'] = """
 type: object
 description: Export data to a SQLite Database
 required:
-  - df
   - database
   - table
 properties:
-  df:
-    type: object
-    description: Pandas Dataframe to be written
   database:
     type: string
-    description: The database to connect to
+    description: The database to connect to including the file path. e.g. directory/database.db
   table:
     type: string
     description: The table to write to
-  action:
-    type: string
-    description: Action to be taken on the database, defaults to 'INSERT'. Options are 'INSERT', 'UPDATE', 'DELETE', etc
-  columns:
-    type: array
-    description: A list with a subset of the columns to import. If omitted, all columns will be written to
 """
