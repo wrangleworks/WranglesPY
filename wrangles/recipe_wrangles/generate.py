e2db6c104179d3690024bd6a8612cbde1641052e
baverkral04 <baverkiral43@gmail.com>
2025-09-24 20:32:03 +0000

    the generate.ai functions variables are being defined

--- wrangles/recipe_wrangles/generate.py
+++ wrangles/recipe_wrangles/generate.py
@@ -2,3 +2,23 @@
 from typing import Union as _Union, Dict as _Dict, List as _List, Optional as _Optional
 import pandas as _pd
 import wrangles.generate as _generate
+
+
+
+def ai(
+    df: _pd.DataFrame,
+    api_key: str,
+    output: _Union[_Dict, str, _List],
+    input: _Union[str, _List] = None,
+    model: str = "gpt-5",
+    threads: int = 20,
+    timeout: int = 90,
+    retries: int = 0,
+    messages: _Optional[_List[dict]] = None,
+    url: str = "https://api.openai.com/v1/responses",
+    strict: bool = False,
+    web_search: bool = False,
+    reasoning: _Dict[str, str] = {"effort": "low"},
+    **kwargs
+) -> _pd.DataFrame:
+
