c41d9747660cceedd5d2b243c74ea9d1a3c81f4a
Thomas K Stover <thomasstvr@gmail.com>
2026-04-06 17:59:40 +0000

    Dropped variable addition in log wrangle

--- wrangles/recipe_wrangles/main.py
+++ wrangles/recipe_wrangles/main.py
@@ -839,12 +839,6 @@ def log(
     variables = kwargs.pop('variables', {})
     variables = _delayed_variable_interpretation(df, variables)
 
-    # Check if variables variable exists, if not add it
-    if 'variables' not in variables:
-        variables['variables'] = variables
-    else:
-        raise ValueError('variables is a reserved variable name and cannot be used as a custom variable')
-
     # Handle variable interpretation for all parameters
     columns, write, error, warning, info = [
         _replace_templated_values(msg, variables) if msg else msg
