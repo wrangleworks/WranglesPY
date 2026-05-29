c6c88a5d1af15438ca63fdd4b6005d676e2619b0
Thomas K Stover <thomasstvr@gmail.com>
2026-05-29 13:10:43 +0000

    Moved BeautifulSoup import into retrieve_metadata function

--- wrangles/search.py
+++ wrangles/search.py
@@ -4,7 +4,6 @@ import json
 import time
 import logging as _logging
 import random
-from bs4 import BeautifulSoup
 
 # Import our client factory
 from .clients import get_client as _get_client
@@ -240,6 +239,15 @@ def retrieve_metadata(
     :param tags_to_keep: A single or list of HTML tag names to keep (e.g., ['script', 'style']). Overrides tags_to_drop.
     :return: tuple: (size_in_bytes (int), cleaned_headers (str), cleaned_html (str))
     """
+    # Attempt to import BeautifulSoup
+    try:
+        from bs4 import BeautifulSoup
+    except ImportError:
+        def BeautifulSoup(*args, **kwargs):
+            raise ImportError(
+                "beautifulsoup4 is required to parse HTML content in "
+                "wrangles.search. Install `beautifulsoup4` to use this functionality."
+            )
     if headers_to_drop and headers_to_keep:
         _logging.info(f"headers_to_keep overriding headers_to_drop")
     if tags_to_drop and tags_to_keep:
