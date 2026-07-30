0ffa75065801b0fde9cbc6cb717719cfbb196aec
Mariia Borodii <mborodii@binariks.com>
2026-07-30 14:52:57 +0000

    update version

--- setup.py
+++ setup.py
@@ -26,7 +26,7 @@ setup(
         'License :: OSI Approved :: Apache Software License',
         'Operating System :: OS Independent'
     ],
-    version = '1.19.4',
+    version = '1.19.5',
     url = 'https://github.com/wrangleworks/WranglesPy',
     author = 'WrangleWorks',
     author_email = 'chris@wrangleworks.com',
