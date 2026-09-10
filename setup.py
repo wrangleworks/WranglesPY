e31865c612a2fdaf0c97cd4ae2a341cbdee9f18b
Eric Hills <53243273+ebhills@users.noreply.github.com>
2026-09-09 19:11:04 -0500

    Bump version from 1.20.3 to 1.20.4

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
         'Operating System :: OS Independent'
     ],
     python_requires = '>=3.11',
-    version = '1.20.3',
+    version = '1.20.4',
     url = 'https://github.com/wrangleworks/WranglesPy',
     author = 'WrangleWorks',
     author_email = 'eric@wrangleworks.com',
